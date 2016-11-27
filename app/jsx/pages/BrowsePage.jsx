aa9283a95c8f6fabbc37badc1c16a4a453755bda
Andy Mardesich <andy.mardesich@ucop.edu>
2016-11-27 09:23:57 -0800

    Statistics for browse campuses page - 1st pass; activeCampuses now orderedby

--- app/jsx/pages/BrowsePage.jsx
+++ app/jsx/pages/BrowsePage.jsx
@@ -13,14 +13,37 @@ class BrowsePage extends PageBase
   }
 
   renderData(data) {
-    let campusList = data.campuses.map(function(c, i) {
-        return c[0] != "" && <p key={i}>{c[1]}</p>
-      })
-    return(
+    let p = data
+    return (
     <div>
       <HeaderComp />
       <NavComp />
-      <BreadcrumbComp array={data.breadcrumb} />
+      <BreadcrumbComp array={p.breadcrumb} />
+      <Content
+        {...p}
+      />
+    </div>
+  )}
+
+}
+
+class Content extends React.Component {
+  render() {
+    let p = this.props
+    return (
+    <div>
+      { p.type == "campuslist" && this.renderCampuses(p) }
+      { p.type == "depts" && this.renderDepts(p) }
+      { p.type == "journals" && this.renderJournals(p) }
+    </div>
+  )}
+
+  renderCampuses(p) {
+    let campusList = p.campuses.map(function(c, i) {
+        return c['id'] != "" && <p key={i}><a href={"/unit/" + c['id']}>{c['name']}</a></p>
+      })
+    return (
+    <div>
       <h2>Campuses</h2>
       <p>
 Lorem ipsum dolor sit amet
@@ -34,6 +57,24 @@ felis tellus mollis orci, sed start a journal nunc eget odio.
     </div>
   )}
 
+  renderJournals(p) {
+    let journalList = p.journals.map(function(j, i) {
+        return <p key={i}><a href={"/unit/" + j["id"]}>{j["name"]}</a></p>
+      })
+    return (
+    <div>
+      <h2>Journals</h2>
+      <p>
+Lorem ipsum dolor sit amet
+
+consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate,
+
+felis tellus mollis orci, sed start a journal nunc eget odio.
+      </p>
+      {journalList}
+      <br/><br/>
+    </div>
+  )}
 }
 
 module.exports = BrowsePage
