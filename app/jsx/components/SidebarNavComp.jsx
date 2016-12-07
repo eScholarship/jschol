6e136aaae5d3aa9dd844acac09905625b5bf5a6e
Martin Haye <martin.haye@ucop.edu>
2016-12-07 15:10:39 -0800

    Refactoring sidebar and static page.

--- app/jsx/components/SidebarNavComp.jsx
+++ app/jsx/components/SidebarNavComp.jsx
@@ -4,26 +4,16 @@ import React from 'react'
 
 class SidebarNavComp extends React.Component {
   render() {
-		return (
-			<nav className="c-sidebarnav">
-				<ul>
-					<li><a href="">About eScholarship</a></li>
-					<li><a href="">About California Digital Library</a></li>
-					<li><a href="">eScholarship Staff</a></li>
-					<li><a href="">Campus Contacts</a></li>
-					<li><a href="">eScholarship Technology</a></li>
-					<li><a href="">Featured Announcements </a></li>
-					<li><a href="">In the Press</a></li>
-					<li><a href="">Press Releases</a></li>
-					<li><a href="">Testimonials</a></li>
-					<li><a href="">OA Policy Information</a></li>
-					<li><a href="">Privacy Policy</a></li>
-					<li><a href="">Recently Added Publications</a></li>
-					<li><a href="">Trending Publications</a></li>
-				</ul>
-			</nav>
-		)
-	}
+    return (
+      <nav className="c-sidebarnav">
+        <ul>
+          {this.props.links.map(link => 
+            <li><Link to={link.to}>{link.text}</Link></li>
+          )}
+        </ul>
+      </nav>
+    )
+  }
 }
 
 module.exports = SidebarNavComp;
--- app/jsx/pages/StaticPage.jsx
+++ app/jsx/pages/StaticPage.jsx
@@ -12,27 +12,28 @@ class StaticPage extends PageBase
     return "/api/static/" + props.params.unitID + "/" + props.params.pageName
   }
 
-  renderData(data) {
-    let p = data
-    return (
+  // PageBase calls this when the API data has been returned to us
+  renderData(data) { return(
     <div>
       <HeaderComp />
       <NavComp />
-      <BreadcrumbComp array={p.breadcrumb} />
+      <BreadcrumbComp array={data.breadcrumb} />
       <div className="c-columns">
         <aside>
           <section className="o-columnbox-sidebar o-sidebarnav">
             <header>
-              <h1 className="o-columnbox-sidebar__heading">About eScholarship</h1>
+              <h1 className="o-columnbox-sidebar__heading">{data.page.title}</h1>
             </header>
-            <SidebarNavComp />
+            <SidebarNavComp links={data.sidebarNavLinks}/>
           </section>
         </aside>
         <main>
           <section className="o-columnbox-main">
             <header>
-              <h1 className="o-columnbox-main__heading">About eScholarship</h1>
+              <h1 className="o-columnbox-main__heading">{data.page.title}</h1>
             </header>
+            {data.page.attrs.html}
+            {/*
             <p>eScholarship provides a suite of open access, scholarly publishing services and research tools that enable departments, research units, publishing programs, and individual scholars associated with the University of California to have direct control over the creation and dissemination of the full range of their scholarship.</p>
             <p>With eScholarship, you can publish the following original scholarly works on a dynamic research platform available to scholars worldwide:</p>
             <ul>
@@ -46,13 +47,12 @@ class StaticPage extends PageBase
             <p>Publications benefit from manuscript and peer-review management systems, as well as a full range of persistent access and preservation services.</p>
             <p>Learn more about what to expect from publishing with eScholarship.</p>
             <p>eScholarship is a service of the Publishing Group of the California Digital Library.</p>
-            <p>Questions? <a className="o-textlink__primary" href="">Contact us</a>.</p>
+            <p>Questions? <a className="o-textlink__primary" href="">Contact us</a>.</p>*/}
           </section>
         </main>
       </div>
     </div>
   )}
-
 }
 
 module.exports = StaticPage;
