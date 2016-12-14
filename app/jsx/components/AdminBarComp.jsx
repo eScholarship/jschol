3cf065f7862fcb650bb403ed11c3f760304b51df
Martin Haye <martin.haye@ucop.edu>
2016-12-14 13:36:59 -0800

    Added an Edit button to a static component.

--- app/jsx/components/AdminBarComp.jsx
+++ app/jsx/components/AdminBarComp.jsx
@@ -10,14 +10,12 @@ class AdminBarComp extends React.Component {
         <div className="c-adminbar__loggedInMsg">
           Logged in as '{p.admin.username}'
         </div>
-        { p.admin.pageHasEditableComponents ?
+        { p.admin.pageHasEditableComponents &&
           <div className="c-adminbar__editPg">
-            { p.admin.editingPage ? 
-                <button className="c-adminbar__editPgButton" onClick={e=>p.admin.onEditingPageChange(false)}>Done editing</button>
-              : <button className="c-adminbar__editPgButton" onClick={e=>p.admin.onEditingPageChange(true)}>Edit page</button>
-            }
+            <button className="c-adminbar__editPgButton" onClick={e=>p.admin.onEditingPageChange(!p.admin.editingPage)}>
+              {p.admin.editingPage ? "Done editing" : "Edit page"}
+            </button>
           </div>
-          : null
         }
       </div>
     )
--- app/jsx/pages/StaticPage.jsx
+++ app/jsx/pages/StaticPage.jsx
@@ -21,8 +21,7 @@ class StaticPage extends PageBase
   }
 
   // PageBase calls this when the API data has been returned to us
-  renderData(data) { 
-    return(
+  renderData(data) { return(
     <div className="l-about">
       <HeaderComp admin={this.state.admin} />
       <NavComp />
@@ -37,27 +36,9 @@ class StaticPage extends PageBase
           </section>
         </aside>
         <main>
-          <section className="o-columnbox1">
-            <header>
-              <h1 className="o-columnbox1__heading">{data.page.title}</h1>
-            </header>
-            <div dangerouslySetInnerHTML={{__html: data.page.html}}/>
-            {/*
-            <p>eScholarship provides a suite of open access, scholarly publishing services and research tools that enable departments, research units, publishing programs, and individual scholars associated with the University of California to have direct control over the creation and dissemination of the full range of their scholarship.</p>
-            <p>With eScholarship, you can publish the following original scholarly works on a dynamic research platform available to scholars worldwide:</p>
-            <ul>
-              <li><a className="o-textlink__primary" href="">Books</a></li>
-              <li><a className="o-textlink__primary" href="">Journals</a></li>
-              <li><a className="o-textlink__primary" href="">Working Papers</a></li>
-              <li><a className="o-textlink__primary" href="">Previously Published Works</a></li>
-              <li><a className="o-textlink__primary" href="">Conferences</a></li>
-            </ul>
-            <p>eScholarship also provides deposit and dissemination services for postprints, or previously published articles.</p>
-            <p>Publications benefit from manuscript and peer-review management systems, as well as a full range of persistent access and preservation services.</p>
-            <p>Learn more about what to expect from publishing with eScholarship.</p>
-            <p>eScholarship is a service of the Publishing Group of the California Digital Library.</p>
-            <p>Questions? <a className="o-textlink__primary" href="">Contact us</a>.</p>*/}
-          </section>
+          <Editable admin={this.state.admin}>
+            <StaticContent {...data.page}/>
+          </Editable>
         </main>
         <aside>
           <section className="o-columnbox2 c-sidebarnav">
@@ -65,7 +46,7 @@ class StaticPage extends PageBase
               <h1 className="o-columnbox2__heading">Featured Articles</h1>
             </header>
             <nav className="c-sidebarnav">
-              Foo bar
+              Lorem ipsum
             </nav>
           </section>
           <section className="o-columnbox2 c-sidebarnav">
@@ -73,12 +54,35 @@ class StaticPage extends PageBase
               <h1 className="o-columnbox2__heading">New Journal Issues</h1>
             </header>
             <nav className="c-sidebarnav">
-              Foo bar
+              Lorem ipsum
             </nav>
           </section>
         </aside>
       </div>
     </div>
+  )}
+}
+
+class Editable extends React.Component
+{
+  render() { return(
+    <div style={{position: "relative"}}>
+      { this.props.children }
+      { this.props.admin.editingPage && 
+        <button style={{position: "absolute", right: "1em", bottom: "1em"}}>Edit</button> }
+    </div>
+  )}
+}
+
+class StaticContent extends React.Component
+{
+  render() { return(
+    <section className="o-columnbox1">
+      <header>
+        <h1 className="o-columnbox1__heading">{this.props.title}</h1>
+      </header>
+      <div dangerouslySetInnerHTML={{__html: this.props.html}}/>
+    </section>
   )}
 }
 
