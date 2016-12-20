74af8d1502509eb510db67e54e80d8f0f62200d6
Martin Haye <martin.haye@ucop.edu>
2016-12-19 16:16:51 -0800

    More advanced webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
+const webpack = require("webpack");
+const _ = require('lodash');
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// read package.json and get dependencies' package ids
+function getNPMPackageIds() {
+  var packageManifest = {};
+  try {
+    packageManifest = require('./package.json');
+  } catch (e) {
+    // does not have a package.json manifest
+  }
+  return _.keys(packageManifest.dependencies) || [];
+}
+
 module.exports = {
-  entry: './app/jsx/App.jsx',
-  output: {
-    filename: 'bundle.js',
-    path: './app/js'
+  entry: {
+    app: './app/jsx/App.jsx',
+    lib: getNPMPackageIds()
   },
+  output: {
+    filename: '[name]-bundle.js',
+    path: './app/js',
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin("lib")
+  ],
   module: {
     loaders: [{
       test: /\.jsx$/,
