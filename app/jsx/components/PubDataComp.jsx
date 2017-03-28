5e6bae4cce27976f5a6fd87ffc23868105d7b67a
Andy Mardesich <andy.mardesich@ucop.edu>
2017-03-28 12:01:30 -0700

    PubDataComp activated

--- app/jsx/components/PubDataComp.jsx
+++ app/jsx/components/PubDataComp.jsx
@@ -6,10 +6,11 @@ class PubDataComp extends React.Component {
   render() {
     return (
       <div className="c-pubdata">
-      {/* ToDo: Include this text only when content_type==null for no-content items. */}
-        **** (UNWIRED) No data is associated with this publication. ****
-      {/* If statement shouldn't appear, at least put in a nsbp, otherwise it makes for overlapping CSS blocks */}
-        &nbsp;
+      {this.props.content_type ?
+        <span>&nbsp;</span>
+        : 
+        <span>No data is associated with this publication.</span>
+      }
       </div>
     )
   }
