41154a9e9bfc881a45d5622b456c73749b3c9e81
Martin Haye <martin.haye@ucop.edu>
2017-04-07 17:04:58 -0700

    Fixed Search2Comp behavior.

--- app/jsx/components/Search2Comp.jsx
+++ app/jsx/components/Search2Comp.jsx
@@ -17,19 +17,21 @@ import Form from 'react-router-form'
 class SearchControls extends React.Component {
   render() {
     let p = this.props,
-      searchUnit = null;
+        searchUnit = null;
 
     if (p.unitID) {
       searchUnit = [
-          <input key="r2" type="radio" id="c-search2__refine-campus" name="searchType" value={p.unitID} onFocus={this.props.makeActive} onBlur={this.props.makeInactive}/>,
-          <label key="l2" htmlFor="c-search_2_refine-campus">This {p.label}</label>
-          ];
+        <input key="r2" type="radio" id="c-search2__refine-campus" name="searchType" value={p.unitID}
+               onClick={this.props.handleRadioSelect} onFocus={this.props.makeActive} onBlur={this.props.makeInactive}/>,
+        <label key="l2" htmlFor="c-search2__refine-campus">This {p.label}</label>
+      ]
     }
 
     return (
     <div className={this.props.refineActive ? "c-search2__refine--active" : "c-search2__refine"}>
-      <input key="r1" type="radio" id="c-search2__refine-eschol" name="searchType" value="eScholarship" defaultChecked={true} onFocus={this.props.makeActive} onBlur={this.props.makeInactive}/>
-      <label key="l1" htmlFor="c-search2__refine-eschol">All of eScholarship</label>
+      <input key="r1" type="radio" id="c-search2__refine-eschol" name="searchType" value="eScholarship" defaultChecked={true}
+             onClick={this.props.handleRadioSelect} onFocus={this.props.makeActive} onBlur={this.props.makeInactive}/>
+      <label key="l1" htmlFor="c-search2__refine-eschol" >All of eScholarship</label>
       { searchUnit }
     </div>
   )}
@@ -41,7 +43,6 @@ class SearchComp2 extends React.Component {
   makeInactive = ()=> this.setState({refineActive: false})
 
   handleRadioSelect = event => {
-    console.log("radio select:", event.target.value)
     if (event.target.value == 'eScholarship') {
       this.setState({search: '/search'})
     } else {
@@ -62,12 +63,13 @@ class SearchComp2 extends React.Component {
     }
 
     return (
-        <Form to={this.state.search} method="GET" onSubmit = {()=> this.setState({refineActive: false})}>
+        <Form to={this.state.search} method="GET">
           <div className="c-search2">
             <div className="c-search2__inputs">
               <div className="c-search2__form">
                   <label className="c-search2__label" htmlFor="global-search">Search eScholarship</label>
-                  <input type="search" name="q" id="global-search" className="c-search2__field" placeholder="Search" onFocus={this.makeActive} onBlur={this.makeInactive}/>
+                  <input type="search" name="q" id="global-search" className="c-search2__field" placeholder="Search" 
+                         onFocus={this.makeActive} onBlur={this.makeInactive}/>
               </div>
               <SearchControls refineActive={this.state.refineActive}
                               handleRadioSelect={this.handleRadioSelect}
