5ec399639e5ef85e95f871657ee51f5ab5adea55
Martin Haye <martin.haye@ucop.edu>
2017-04-18 10:48:09 -0700

    Committing to yarn instead of npm.

--- setup.sh
+++ setup.sh
@@ -7,15 +7,7 @@ printf "== Installing local Ruby gems ==\n"
 bundle install --path=gems --binstubs
 
 printf "\n== Installing node packages (used by gulp and iso via Node) ==\n"
-set +e
-type yarn 2>/dev/null
-YARNSTATUS=$?
-set -e
-if [[ $YARNSTATUS == "0" ]]; then
-  yarn install
-else
-  npm install
-fi
+yarn install
 
 printf "\n== Installing bower packages (used in browser) ==\n"
 node_modules/.bin/bower install
