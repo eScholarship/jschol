b10fe0f81fb7499f1dfaa61ce1bf887e5c408c0b
Martin Haye <martin.haye@ucop.edu>
2017-06-12 15:46:12 -0700

    Found a workaround for the Flickity vs isomorphic mode problem that led to 'Unable to find element with ID ##' in the console.

--- app/jsx/components/ItemCarouselComp.jsx
+++ app/jsx/components/ItemCarouselComp.jsx
@@ -1,33 +1,18 @@
 // ##### Item Carousel Component ##### //
 
 import React from 'react'
-import $ from 'jquery'
-
-// Only load flickity when in the browser (not server-side)
-if (!(typeof document === "undefined")) {
-  var Flickity = require('flickity-imagesloaded')
-}
+import CarouselComp from '../components/CarouselComp.jsx'
 
 class ItemCarouselComp extends React.Component {
-  componentDidMount () {
-    var carousel = $('.c-itemcarousel__carousel')[0];
-    var options = {
-      cellAlign: 'left',
-      contain: true,
-      initialIndex: 0,
-      pageDots: false
-    }
-    this.flkty = new Flickity(carousel, options);
-  }
-  componentWillUnmount() {
-    if (this.flkty) {
-      this.flkty.destroy();
-    }
-  }
   render() {
     return (
       <div className="c-itemcarousel">
-        <div className="c-itemcarousel__carousel">
+        <CarouselComp className="c-itemcarousel__carousel" options={{
+            cellAlign: 'left',
+            contain: true,
+            initialIndex: 0,
+            pageDots: false
+          }}>
           <div className="c-itemcarousel__item">
             Item 1
           </div>
@@ -50,7 +35,7 @@ class ItemCarouselComp extends React.Component {
         <div className="o-stat--view c-itemcarousel__stats-view">
           <b>100,000</b>Views
         </div>
-      </div>
+      </CarouselComp>
     )
   }
 }
--- app/jsx/components/MarqueeComp.jsx
+++ app/jsx/components/MarqueeComp.jsx
@@ -1,61 +1,37 @@
 // ##### Carousel Component ##### //
 
 import React from 'react'
-import $ from 'jquery'
+import CarouselComp from '../components/CarouselComp.jsx'
 import { Link } from 'react-router'
 
-// Only load flickity when in the browser (not server-side)
-if (!(typeof document === "undefined")) {
-  var Flickity = require('flickity-imagesloaded')
-  const dotdotdot = require('jquery.dotdotdot')
-}
-
 class MarqueeComp extends React.Component {
-  componentDidMount () {
-    if (this.props.marquee.carousel) {
-      var carousel = $('.c-marquee__carousel')[0];
-      var options = {
-        cellAlign: 'left',
-        contain: true,
-        initialIndex: 0,
-        imagesLoaded: true
-      }
-      this.flkty = new Flickity(carousel, options);
-      $(".c-marquee__carousel-cell").css('visibility', 'visible')
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.flkty) {
-      this.flkty.destroy();
-    }
-  }
   render() {
-    var carouselCells;
-    if (this.props.marquee.carousel) {
-      carouselCells = [
-        <div key="1" className="c-marquee__carousel-cell" style={{backgroundImage: "url('https://static.pexels.com/photos/27714/pexels-photo-27714.jpg')"}}>
-           <h2>Carousel Cell Title 1</h2>
-          <p>Magnam praesentium sint, ducimus aspernatur architecto, deserunt ipsa veniam quia nihil, doloribus, laudantium a ad error tenetur fuga consequuntur laboriosam omnis ipsam.</p>
-          <a href="">More&hellip;</a>
-        </div>,
-        <div key="2" className="c-marquee__carousel-cell" style={{visibility: "hidden", backgroundImage: "url('https://static.pexels.com/photos/40797/wild-flowers-flowers-plant-macro-40797.jpeg')"}}>
-          <h2>Carousel Cell Title 2</h2>
-          <p>Iure quod itaque maiores optio eveniet assumenda omnis, similique. Possimus, expedita, ea?</p>
-          <a href="">More&hellip;</a>
-        </div>,
-        <div key="3" className="c-marquee__carousel-cell" style={{visibility: "hidden", backgroundImage: "url('http://www.almanac.com/sites/default/files/birth_month_flowers-primary-1920x1280px_pixabay.jpg')"}}>
-          <h2>Carousel Cell Title 3</h2>
-          <p>Obcaecati consequatur quaerat eaque, beatae eligendi possimus, repudiandae magni quas dolores, sit voluptatem iusto laborum. Incidunt fuga sed dicta nisi voluptates eaque, beatae numquam officia animi, vel.</p>
-          <a href="">More&hellip;</a>
-        </div>
-      ]
-    }
     return (
       <div className="c-marquee">
-        <div className="c-marquee__carousel">
-          {carouselCells}
-        </div>
+        { this.props.marquee.carousel &&
+          <CarouselComp className="c-marquee__carousel" options={{
+              cellAlign: 'left',
+              contain: true,
+              initialIndex: 0,
+              imagesLoaded: true
+            }}>
+            <div className="c-marquee__carousel-cell" style={{backgroundImage: "url('https://static.pexels.com/photos/27714/pexels-photo-27714.jpg')"}}>
+               <h2>Carousel Cell Title 1</h2>
+              <p>Magnam praesentium sint, ducimus aspernatur architecto, deserunt ipsa veniam quia nihil, doloribus, laudantium a ad error tenetur fuga consequuntur laboriosam omnis ipsam.</p>
+              <a href="">More&hellip;</a>
+            </div>
+            <div className="c-marquee__carousel-cell" style={{backgroundImage: "url('https://static.pexels.com/photos/40797/wild-flowers-flowers-plant-macro-40797.jpeg')"}}>
+              <h2>Carousel Cell Title 2</h2>
+              <p>Iure quod itaque maiores optio eveniet assumenda omnis, similique. Possimus, expedita, ea?</p>
+              <a href="">More&hellip;</a>
+            </div>
+            <div className="c-marquee__carousel-cell" style={{backgroundImage: "url('http://www.almanac.com/sites/default/files/birth_month_flowers-primary-1920x1280px_pixabay.jpg')"}}>
+              <h2>Carousel Cell Title 3</h2>
+              <p>Obcaecati consequatur quaerat eaque, beatae eligendi possimus, repudiandae magni quas dolores, sit voluptatem iusto laborum. Incidunt fuga sed dicta nisi voluptates eaque, beatae numquam officia animi, vel.</p>
+              <a href="">More&hellip;</a>
+            </div>
+          </CarouselComp>
+        }
         { this.props.marquee.about &&
           <aside className="c-marquee__sidebar">
             <section className="o-columnbox2">
