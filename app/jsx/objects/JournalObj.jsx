0fd481c6606e13de4b30c51ab97608881cc509b9
Andy Mardesich <andy.mardesich@ucop.edu>
2017-06-26 12:22:54 -0700

    Integrated new eschol-ui version from Joel

--- app/jsx/objects/JournalObj.jsx
+++ app/jsx/objects/JournalObj.jsx
@@ -1,8 +1,15 @@
 // ##### Journal Objects ##### //
 
 import React from 'react'
+import $ from 'jquery'
+import dotdotdot from 'jquery.dotdotdot'
 
 class JournalObj extends React.Component {
+  componentDidMount () {
+    $('.o-journal2 figcaption').dotdotdot({
+      watch: 'window'
+    });
+  }
   render() {
     return (
       <div>
@@ -10,8 +17,16 @@ class JournalObj extends React.Component {
         <h2>Featured Journal</h2>
         <a href="" className="o-journal1">
           <figure>
-            <img src="images/sample_journal1.png" alt="sample journal"/>
+            <img src="http://escholarship.org/issueCovers/uclalaw_cllr/34_01_cover.png" alt=""/>
             <figcaption>Chicana-Latina Law Review</figcaption>
+          </figure>
+        </a>
+
+        <h2>Journal Item</h2>
+        <a href="" className="o-journal2">
+          <figure>
+            <img src="http://escholarship.org/issueCovers/jmie_sfews/15_01_cover.png" alt=""/>
+            <figcaption>The Proceedings of the UCLA Department of Spanish and Portuguese Graduate Conference</figcaption>
           </figure>
         </a>
 
