3db5784207cafdbbce8d15bd70e252e248154dad
Andy Mardesich <andy.mardesich@ucop.edu>
2017-06-30 08:33:37 -0700

    Campus landing: Unit Carousel dummified

--- app/jsx/components/UnitCarouselComp.jsx
+++ app/jsx/components/UnitCarouselComp.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import $ from 'jquery'
 import PropTypes from 'prop-types'
 import CarouselComp from '../components/CarouselComp.jsx'
+import { Link } from 'react-router'
 
 // Load dotdotdot in browser but not server
 if (!(typeof document === "undefined")) {
@@ -12,33 +13,35 @@ if (!(typeof document === "undefined")) {
 
 class UnitCarouselItem extends React.Component {
   static propTypes = {
-    orientation: PropTypes.string.isRequired,
     result: PropTypes.shape({
       id: PropTypes.string,
       genre: PropTypes.string,
       title: PropTypes.string,
       authors: PropTypes.array,
     }).isRequired,
+    orientation: PropTypes.string.isRequired,
   }
   render() {
-    let p = this.props.result
+    let pr = this.props.result,
+        itemLink = "/uc/item/"+pr.id.replace(/^qt/, "")
+    let authorList
+    if (pr.authors) {
+      // Joel's CSS handles inserting semicolons here.
+      authorList = pr.authors.map(function(author, i) {
+        return (<li key={i}>{author.name}</li>)
+      })
+    }
     return (
       <div className="o-itemcarousel__item">
-          <a href="" className={"o-unititem--" + this.props.orientation}>
-            <div className="o-unititem__type--article">{p.genre}</div>
-            <div className="o-unititem__title">{p.title}</div>
+          <Link to={itemLink} className={"o-unititem--" + this.props.orientation}>
+            <div className="o-unititem__type--article">{pr.genre}</div>
+            <div className="o-unititem__title">{pr.title}</div>
+          {authorList &&
             <ul className="o-unititem__author">
-              <li>Fung, Joe</li>
-              <li>Wu, Abe</li>
-              <li>Reed, Laura K</li>
-              <li>Smith, Sheryl T</li>
-              <li>Barshop, William</li>
-              <li>Wong, Jeannette</li>
-              <li>Dothager, Matthew</li>
-              <li>Lee, Paul</li>
-              <li>Wong, Jeannette</li>
+              {authorList}
             </ul>
-          </a>
+          }
+          </Link>
         </div>
     )
   }
@@ -52,9 +55,17 @@ class UnitCarouselComp extends React.Component {
     });
   }
   render() {
+    let dummyData = ([
+{"id":"qt9v77h5zm","title":"Distinct configurations of protein complexes and biochemical pathways revealed by epistatic interaction network motifs","authors":[{"name":"Peet, Alison","fname":"Alison","lname":"Peet"},{"name":"Tyroler-Cooper, Samm","fname":"Samm","lname":"Tyroler-Cooper"},{"name":"Tsang, Daniel","email":"dtsang@uci.edu","fname":"Daniel","lname":"Tsang","institution":"UC Irvine"},{"name":"Liang, Wei","fname":"Wei","lname":"Liang","institution":"University of Southern California"}],"genre":"article"},
+{"id":"qt9j48n0p8","title":"China’s contingencies and globalisation","authors":[{"name":"Nederveen Pieterse, J","email":"jnp@global.ucsb.edu","fname":"J","lname":"Nederveen Pieterse"}],"genre":"article"},
+{"id":"qt5zt6n277","title":"Japan's China Strategy","authors":[{"name":"TAKAHASHI, Sugio","fname":"Sugio","lname":"TAKAHASHI"}],"genre":"article"},
+{"id":"qt0zd563ks","title":"China’s Rise as a Global S&T Power and China–EU Cooperation","authors":[{"name":"Bräuner, Oliver","fname":"Oliver","lname":"Bräuner"}],"genre":"article"},
+{"id":"qt0dk9k1rb","title":"How (and Why) to Do Business with China","authors":[{"name":"Tsang, Daniel","email":"dtsang@uci.edu","fname":"Daniel","lname":"Tsang","institution":"UC Irvine"}],"genre":"article"}
+])
+
     return (
       <div className="o-itemcarousel">
-        <h2 className="o-itemcarousel__heading">Center for Medieval and Renaissance Studies</h2>
+        <h2 className="o-itemcarousel__heading"><Link to="/uc/crb">Center for Medieval and Renaissance Studies</Link></h2>
         <CarouselComp className="c-unitcarousel o-itemcarousel__carousel"
                       options={{
                         cellAlign: 'left',
@@ -62,12 +73,12 @@ class UnitCarouselComp extends React.Component {
                         pageDots: false,
                         percentPosition: false // px instead of % cells
                       }}>
-          { data.unitItems.map((result, i) =>
-            <UnitCarouselItem key={i} orientation={(i+1)%2 ? 'vert':'horz'} result={result} />)
+          { dummyData.map((result, i) =>
+            <UnitCarouselItem key={i} result={result} orientation={(i+1)%2 ? 'vert':'horz'} />)
           }
         </CarouselComp>
         <div className="o-stat--item o-itemcarousel__stats-item">
-          <b>1,000</b>Items
+          <Link to="/uc/crb"><b>1,000</b></Link>Items
         </div>
         <div className="o-stat--view o-itemcarousel__stats-view">
           <b>100,000</b>Views
