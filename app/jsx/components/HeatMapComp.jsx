86f30892a475567ac21098ac0635c9cd82fd4793
Andy Mardesich <andy.mardesich@ucop.edu>
2017-06-29 19:23:28 -0700

    NotYetLink (Coming Soon)

--- app/jsx/components/HeatMapComp.jsx
+++ app/jsx/components/HeatMapComp.jsx
@@ -1,13 +1,14 @@
 // ##### Heat Map Component ##### //
 
 import React from 'react'
+import NotYetLink from '../components/NotYetLink.jsx'
 
 class HeatMapComp extends React.Component {
   render() {
     return (
       <div className="c-heatmap" style={{backgroundImage: "url('/images/sample_heatmap.png')"}}>
         <h1 className="c-heatmap__heading">Share your research with a <br/><strong>global audience</strong></h1>
-        <button className="c-heatmap__button">Get Started</button>
+        <NotYetLink><button className="c-heatmap__button">Get Started</button></NotYetLink>
         <div className="c-heatmap__text">
           The eScholarship suite of open access publishing services gives UC scholars direct control over the creation and dissemination of the full range of their research.
         </div>
--- app/jsx/layouts/CampusLayout.jsx
+++ app/jsx/layouts/CampusLayout.jsx
@@ -5,6 +5,7 @@ import HeatMapComp from '../components/HeatMapComp.jsx'
 import StatCarouselComp from '../components/StatCarouselComp.jsx'
 import UnitCarouselComp from '../components/UnitCarouselComp.jsx'
 import JournalCarouselComp from '../components/JournalCarouselComp.jsx'
+import NotYetLink from '../components/NotYetLink.jsx'
 import { Link } from 'react-router'
 
 class CampusLayout extends React.Component {
