441d46a1882f5c9cc8d16ae4fc65132ec40610af
Andy Mardesich <andy.mardesich@ucop.edu>
2017-06-29 20:17:08 -0700

    UnitCarousel integration started

--- app/jsx/components/UnitCarouselComp.jsx
+++ app/jsx/components/UnitCarouselComp.jsx
@@ -2,10 +2,46 @@
 
 import React from 'react'
 import $ from 'jquery'
+import PropTypes from 'prop-types'
 import CarouselComp from '../components/CarouselComp.jsx'
+
 // Load dotdotdot in browser but not server
 if (!(typeof document === "undefined")) {
   const dotdotdot = require('jquery.dotdotdot')
+}
+
+class UnitCarouselItem extends React.Component {
+  static propTypes = {
+    orientation: PropTypes.string.isRequired,
+    result: PropTypes.shape({
+      id: PropTypes.string,
+      genre: PropTypes.string,
+      title: PropTypes.string,
+      authors: PropTypes.array,
+    }).isRequired,
+  }
+  render() {
+    let p = this.props.result
+    return (
+      <div className="o-itemcarousel__item">
+          <a href="" className={"o-unititem--" + this.props.orientation}>
+            <div className="o-unititem__type--article">{p.genre}</div>
+            <div className="o-unititem__title">{p.title}</div>
+            <ul className="o-unititem__author">
+              <li>Fung, Joe</li>
+              <li>Wu, Abe</li>
+              <li>Reed, Laura K</li>
+              <li>Smith, Sheryl T</li>
+              <li>Barshop, William</li>
+              <li>Wong, Jeannette</li>
+              <li>Dothager, Matthew</li>
+              <li>Lee, Paul</li>
+              <li>Wong, Jeannette</li>
+            </ul>
+          </a>
+        </div>
+    )
+  }
 }
 
 class UnitCarouselComp extends React.Component {
@@ -26,91 +62,9 @@ class UnitCarouselComp extends React.Component {
                         pageDots: false,
                         percentPosition: false // px instead of % cells
                       }}>
-          <div className="o-itemcarousel__item">
-            <a href="" className="o-unititem--vert">
-              <div className="o-unititem__type--article">Article</div>
-              <div className="o-unititem__title">Libero doloremque suscipit perferendis amet nostrum! Nostrum quisquam, tempore voluptatum ea dolor, hic esse adipisci reprehenderit ullam minima distinctio. Vero, molestias non.</div>
-              <ul className="o-unititem__author">
-                <li>Fung, Joe</li>
-                <li>Wu, Abe</li>
-                <li>Reed, Laura K</li>
-                <li>Smith, Sheryl T</li>
-                <li>Barshop, William</li>
-                <li>Wong, Jeannette</li>
-                <li>Dothager, Matthew</li>
-                <li>Lee, Paul</li>
-                <li>Wong, Jeannette</li>
-              </ul>
-            </a>
-          </div>
-          <div className="o-itemcarousel__item">
-            <a href="" className="o-unititem--horz">
-              <div className="o-unititem__type--book">Book</div>
-              <div className="o-unititem__title">Sapiente pariatur voluptatibus, quisquam quam libero aspernatur esse dolorem, nisi voluptate accusantium consectetur temporibus maiores quis tempore. Non, architecto eveniet a laudantium.</div>
-              <ul className="o-unititem__author">
-                <li>Fung, Joe</li>
-                <li>Wu, Abe</li>
-                <li>Reed, Laura K</li>
-                <li>Smith, Sheryl T</li>
-                <li>Barshop, William</li>
-                <li>Wong, Jeannette</li>
-                <li>Dothager, Matthew</li>
-                <li>Lee, Paul</li>
-                <li>Wong, Jeannette</li>
-              </ul>
-            </a>
-          </div>
-          <div className="o-itemcarousel__item">
-            <a href="" className="o-unititem--vert">
-              <div className="o-unititem__type--multimedia">Multimedia</div>
-              <div className="o-unititem__title">Libero doloremque suscipit perferendis amet nostrum! Nostrum quisquam, tempore voluptatum ea dolor, hic esse adipisci reprehenderit ullam minima distinctio. Vero, molestias non.</div>
-              <ul className="o-unititem__author">
-                <li>Fung, Joe</li>
-                <li>Wu, Abe</li>
-                <li>Reed, Laura K</li>
-                <li>Smith, Sheryl T</li>
-                <li>Barshop, William</li>
-                <li>Wong, Jeannette</li>
-                <li>Dothager, Matthew</li>
-                <li>Lee, Paul</li>
-                <li>Wong, Jeannette</li>
-              </ul>
-            </a>
-          </div>
-          <div className="o-itemcarousel__item">
-            <a href="" className="o-unititem--horz">
-              <div className="o-unititem__type--thesis">Thesis</div>
-              <div className="o-unititem__title">Sapiente pariatur voluptatibus, quisquam quam libero aspernatur esse dolorem, nisi voluptate accusantium consectetur temporibus maiores quis tempore. Non, architecto eveniet a laudantium.</div>
-              <ul className="o-unititem__author">
-                <li>Fung, Joe</li>
-                <li>Wu, Abe</li>
-                <li>Reed, Laura K</li>
-                <li>Smith, Sheryl T</li>
-                <li>Barshop, William</li>
-                <li>Wong, Jeannette</li>
-                <li>Dothager, Matthew</li>
-                <li>Lee, Paul</li>
-                <li>Wong, Jeannette</li>
-              </ul>
-            </a>
-          </div>
-          <div className="o-itemcarousel__item">
-            <a href="" className="o-unititem--vert">
-              <div className="o-unititem__type--article">Article</div>
-              <div className="o-unititem__title">Libero doloremque suscipit perferendis amet nostrum! Nostrum quisquam, tempore voluptatum ea dolor, hic esse adipisci reprehenderit ullam minima distinctio. Vero, molestias non.</div>
-              <ul className="o-unititem__author">
-                <li>Fung, Joe</li>
-                <li>Wu, Abe</li>
-                <li>Reed, Laura K</li>
-                <li>Smith, Sheryl T</li>
-                <li>Barshop, William</li>
-                <li>Wong, Jeannette</li>
-                <li>Dothager, Matthew</li>
-                <li>Lee, Paul</li>
-                <li>Wong, Jeannette</li>
-              </ul>
-            </a>
-          </div>
+          { data.unitItems.map((result, i) =>
+            <UnitCarouselItem key={i} orientation={(i+1)%2 ? 'vert':'horz'} result={result} />)
+          }
         </CarouselComp>
         <div className="o-stat--item o-itemcarousel__stats-item">
           <b>1,000</b>Items
