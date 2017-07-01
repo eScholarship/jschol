753cacb32a5f4f96bb6ac9c45130260e2efc8d32
Andy Mardesich <andy.mardesich@ucop.edu>
2017-06-30 18:00:35 -0700

    NotYetLink styling fixed

--- app/jsx/components/InfoPagesComp.jsx
+++ app/jsx/components/InfoPagesComp.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import $ from 'jquery'
+import NotYetLink from '../components/NotYetLink.jsx'
 
 // Load dotdotdot in browser but not server
 if (!(typeof document === "undefined")) {
@@ -18,30 +19,30 @@ class InfoPagesComp extends React.Component {
         <div className="c-infopages__items">
           <div className="c-infopages__item">
             <h2>
-              <a href="">The Center for Environmental Design Research</a>
-              <a href="" className="c-infopages__title">In the Press</a>
+              <NotYetLink element="a">The Center for Environmental Design Research</NotYetLink>
+              <NotYetLink className="c-infopages__title" element="a">In the Press</NotYetLink>
             </h2>
             <div className="c-infopages__text">The University of California awards contract to Symplectic for the implementation of a publication harvesting system to support UC’s Open Access Policy" March, 2014
             </div>
           </div>
           <div className="c-infopages__item">
             <h2>
-              <a href="">eScholarship</a>
-              <a href="" className="c-infopages__title">Lorem Ipsum Dolor Sit Amet</a>
+              <NotYetLink element="a">eScholarship</NotYetLink>
+              <NotYetLink element="a" className="c-infopages__title">Lorem Ipsum Dolor Sit Amet</NotYetLink>
             </h2>
             <div className="c-infopages__text">Debitis incidunt aliquam nemo iure alias, amet rerum velit quidem. Quia, provident voluptatum earum mollitia aliquam at nulla quam, sunt! Laboriosam est perspiciatis molestias excepturi ad repellendus.
             </div>
           </div>
           <div className="c-infopages__item">
             <h2>
-              <a href="">UC Berkeley</a>
-              <a href="" className="c-infopages__title">Consectetur Adipisicing Elit Delectus Veritatis Mollitia</a>
+              <NotYetLink element="a">UC Berkeley</NotYetLink>
+              <NotYetLink element="a" className="c-infopages__title">Consectetur Adipisicing Elit Delectus Veritatis Mollitia</NotYetLink>
             </h2>
             <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide.
             </div>
           </div>
         </div>
-        <a href="" className="c-infopages__all">Show all informational page results</a>
+        <NotYetLink element="a" className="c-infopages__all">Show all informational page results</NotYetLink>
       </div>
     )
   }
--- app/jsx/components/NotYetLink.jsx
+++ app/jsx/components/NotYetLink.jsx
@@ -3,6 +3,18 @@
 import React from 'react'
 import ModalComp from '../components/ModalComp.jsx'
 
+class ModalNotYetComp extends React.Component {
+  content = (<div>Ahem...This page or functionality is not yet working.<br/>We appreciate your patience.</div>)
+  render () {
+    return (
+      <ModalComp isOpen={this.props.isOpen}
+         header="Sorry!"
+         content={this.content}
+         onOK={this.props.onOK} okLabel="OK" />
+    )
+  }
+}
+
 class NotYetLink extends React.Component {
   state = { modalOpen: false }
 
@@ -10,22 +22,25 @@ class NotYetLink extends React.Component {
     this.setState({modalOpen:false})
   }
 
-  content = (<div>Ahem...This page or functionality is not yet working.<br/>We appreciate your patience.</div>)
+  clickHandler = event =>{
+    this.setState({modalOpen:true})
+    event.preventDefault()
+  }
+
 
   render() {
+    let element = this.props.element
     return (
-      <div id="notYetLinkModalBase">
-        <a href="" onClick={(event)=>{
-          this.setState({modalOpen:true})
-          event.preventDefault()}}>
-          {this.props.children}
-        </a>
-        <ModalComp isOpen={this.state.modalOpen}
-          parentSelector={()=>$("#notYetLinkModalBase")[0]}
-          header="Sorry!"
-          content={this.content}
-          onOK={e=>this.closeModal(e)} okLabel="OK" />
-      </div>
+     element == "button" ?
+       <button key="1" className={this.props.className} onClick={(e)=>this.clickHandler(e)}>
+         {this.props.children}
+         <ModalNotYetComp isOpen={this.state.modalOpen} onOK={e=>this.closeModal(e)}/>
+       </button>
+     :
+       <a href="" className={this.props.className} onClick={(e)=>this.clickHandler(e)}>
+         {this.props.children}
+         <ModalNotYetComp isOpen={this.state.modalOpen} onOK={e=>this.closeModal(e)}/>
+       </a>
     )
   }
 }
