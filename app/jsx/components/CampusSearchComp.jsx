9f1234c5a97343c6efca378be589bb6c2f6a168d
Andy Mardesich <andy.mardesich@ucop.edu>
2017-07-20 11:47:43 -0700

    Campus landing page search fixed

--- app/jsx/components/CampusSearchComp.jsx
+++ app/jsx/components/CampusSearchComp.jsx
@@ -5,13 +5,14 @@ import Form from 'react-router-form'
 
 class CampusSearchComp extends React.Component {
   render() {
-    let search = '/uc/' + this.props.campusID + '/search'
     return (
-    <Form to={search} method="GET">
+    <Form to="/search" method="GET">
       <div className="c-campussearch">
         <label htmlFor="c-campussearch__search" className="c-campussearch__label">Discover {this.props.campusName} scholarship</label>
         <div className="c-campussearch__search">
           <input type="search" name="q" className="c-campussearch__input" id="c-campussearch__search"/>
+          <input type="hidden" name="searchUnitType" value="campuses" />
+          <input type="hidden" name="searchType" value={this.props.campusID} />
           <button type="submit" className="c-campussearch__button" aria-label="Search"></button>
         </div>
       {this.props.dashUrl &&
