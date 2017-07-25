ee0f88629c2ef846f1b00bf0ec3a64be74b8e764
Martin Haye <martin.haye@ucop.edu>
2017-07-25 12:12:25 -0700

    Getting rid of public domain, since we don't support it in any way.

--- app/jsx/components/RightsComp.jsx
+++ app/jsx/components/RightsComp.jsx
@@ -9,7 +9,6 @@ class RightsComp extends React.Component {
         baseURL = "https://creativecommons.org",
         lv = "4.0",
         rightsMap = {
-        'public':      [`${baseURL}/publicdomain/zero/1.0/`,   `cc-zero-${size}.svg`,     "CC0 1.0 Universal (Public Domain)"],
         'CC BY':       [`${baseURL}/licenses/by/${lv}/`,       `cc-by-${size}.svg`,       "Attribution 4.0 International Public License"],
         'CC BY-NC':    [`${baseURL}/licenses/by-nc/${lv}/`,    `cc-by-nc-${size}.svg`,    "Attribution-NonCommercial 4.0 International Public License"],
         'CC BY-NC-ND': [`${baseURL}/licenses/by-nc-nd/${lv}/`, `cc-by-nc-nd-${size}.svg`, "Attribution-NonCommercial-NoDerivatives 4.0 International Public License"],
