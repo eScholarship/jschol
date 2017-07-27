f4fe29c836ace531f69ce12be239789cc518a4ef
Martin Haye <martin.haye@ucop.edu>
2017-07-26 17:08:18 -0700

    Better commentary.

--- tools/copyPagesFromDev.rb
+++ tools/copyPagesFromDev.rb
@@ -17,10 +17,12 @@ puts "Dropping old eschol_fromdev db (ignore error if any)."
 puts "Creating eschol_fromdev db."
 `echo "create database eschol_fromdev" | mysql --defaults-extra-file=/apps/eschol/.passwords/eschol_dba_pw.mysql`
 
-puts "Copying all data from dev."
+puts "Copying all data from dev (takes about 2 min)."
 `ssh pub-jschol-dev.escholarship.org "mysqldump --defaults-extra-file=/apps/eschol/.passwords/eschol_dump_pw.mysql --skip-lock-tables eschol_test" | mysql --defaults-extra-file=/apps/eschol/.passwords/eschol_dba_pw.mysql --database eschol_fromdev`
 
-puts "Copying root pages"
+puts "Copying root pages."
 `echo "update units u1 set attrs = (select attrs from eschol_fromdev.units u2 where u2.id = u1.id) where type in ('root', 'campus')" | mysql --defaults-extra-file=/apps/eschol/.passwords/eschol_dba_pw.mysql`
 `echo "delete from pages where unit_id in (select id from units where type in ('root', 'campus'))" | mysql --defaults-extra-file=/apps/eschol/.passwords/eschol_dba_pw.mysql`
 `echo "insert into pages (unit_id, name, title, slug, attrs) select unit_id, name, title, slug, attrs from eschol_fromdev.pages where unit_id in (select id from eschol_fromdev.units where type in ('root', 'campus'))" | mysql --defaults-extra-file=/apps/eschol/.passwords/eschol_dba_pw.mysql`
+
+puts "Done."
