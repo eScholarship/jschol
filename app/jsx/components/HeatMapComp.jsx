39f6a005dc283f867b89ef6fff9035cfcd94e2d1
Andy Mardesich <andy.mardesich@ucop.edu>
2017-09-22 10:59:00 -0700

    temporary placeholder Campus hero image

--- app/jsx/components/HeatMapComp.jsx
+++ app/jsx/components/HeatMapComp.jsx
@@ -5,8 +5,9 @@ import { Link, browserHistory } from 'react-router'
 
 class HeatMapComp extends React.Component {
   render() {
+    let heroImage = "/images/herocampus_" + this.props.campusID + ".jpg"
     return (
-      <div className="c-heatmap" style={{backgroundImage: "url('/images/world-map.svg')"}}>
+      <div className="c-heatmap" style={{backgroundImage: "url(" + heroImage + ")"}}>
         <h1 className="c-heatmap__heading">Share your research with a <br/>global audience</h1>
         <button onClick={(e)=>{browserHistory.push("/uc/"+this.props.campusID+"/about")}} className="c-heatmap__button">Get Started</button>
         <div className="c-heatmap__text">
