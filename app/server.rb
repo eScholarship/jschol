a77c8a2e13704db7a6ebaf6a97734d92d071ab0c
Martin Haye <martin.haye@ucop.edu>
2017-10-13 14:24:03 -0700

    Making good progress on redirects, esp bepress and doj.

--- app/server.rb
+++ app/server.rb
@@ -241,18 +241,107 @@ require_relative 'dbCache'
 $ipFilter = File.exist?("config/allowed_ips") && Regexp.new(File.read("config/allowed_ips").strip)
 before do
   $ipFilter && !$ipFilter.match(request.ip) and halt 403
-  if ["www.escholarship.org", "pvw.escholarship.org", "eprints.cdlib.org"].include?(request.host)
-    redirect to("#{request.scheme}://escholarship.org#{request.path}#{request.query_string.empty? ? "" : "?#{request.query_string}"}"), 301
-  elsif request.path =~ %r{/uc/item/([^/]+)}
-    itemID, remainder = $1, $2
-    if (redir = Redirect.where(kind: "item", from_path: "/uc/item/#{itemID}").first)
-      redirect to("#{request.scheme}://escholarship.org#{redir.to_path}#{remainder}"), 301
-    elsif !request.query_string.empty?
-      redirect to("#{request.scheme}://escholarship.org/uc/item/#{itemID}"), 301
-    elsif remainder =~ %r{/.*} && !request.url.include?(request.referer)
-      redirect to("#{request.scheme}://escholarship.org/uc/item/#{itemID}"), 303
+  query = request.query_string.empty? ? "" : "?#{request.query_string}"
+  if request.path.include?(".php")  # We have no PHP on our site. These are almost always attacks anyhow
+    halt 404
+  elsif ["www.escholarship.org", "pvw.escholarship.org", "eprints.cdlib.org", "escholarship.cdlib.org"].include?(request.host)
+    redirect to("https://escholarship.org#{request.path}#{query}"), 301
+  elsif request.path =~ %r{^/uc/item/(\w+)(.*)}
+    return if handleItemRedirect(request, query, $1, $2)
+  elsif request.path =~ %r{^/editions/(.*)}
+    remainder = $1
+    redirect to("https://publishing.cdlib.org/ucpressebooks/#{remainder}#{query}"), 301
+  elsif request.path =~ %r{/uc/search}
+    handleSearchRedirect(request, query)
+  elsif request.path =~ %r{^/uc/([^/]+)(.*)}
+    unit, remainder = $1, $2
+    if (redir = Redirect.where(kind: "unit", from_path: "/uc/#{unit}").first)
+      redirect to("https://escholarship.org#{redir.to_path}#{remainder}"), 301
+    end
+  elsif request.host == "repositories.cdlib.org"
+    handleBepressRedirect(request, query)
+  elsif request.host =~ /dermatology(-s10)?.cdlib.org/
+    handleDojRedirect(request, query)
+  end
+end
+
+###################################################################################################
+def handleItemRedirect(request, query, itemID, remainder)
+  if (redir = Redirect.where(kind: "item", from_path: "/uc/item/#{itemID}").first)
+    redirect to("https://escholarship.org#{redir.to_path}#{remainder}"), 301
+  elsif !request.query_string.empty?
+    redirect to("https://escholarship.org/uc/item/#{itemID}"), 301
+  elsif remainder =~ %r{^/.*} && (request.referer.nil? || !request.url.include?(request.referer))
+    redirect to("https://escholarship.org/uc/item/#{itemID}"), 303
+  elsif remainder =~ %r{^\.pdf}
+    if $cloudFrontConfig
+      redirect to("#{$cloudFrontConfig['public-url']}/content/qt#{itemID}/qt#{itemID}.pdf"), 303
+    else
+      redirect to("https://escholarship.org/content/qt#{itemID}/qt#{itemID}.pdf"), 303
     end
   end
+end
+
+###################################################################################################
+def handleSearchRedirect(request, query)
+  if query =~ %r{\?entity=([\w_]+)$}
+    unit = $1
+    redirect to("https://escholarship.org/uc/#{unit}"), 301
+  elsif query =~ %r{\?entity=([\w_]+);volume=([^;]+);issue=([^;]+)$}
+    unit, vol, iss = $1, $2, $3
+    redirect to("https://escholarship.org/uc/#{unit}/#{vol}/#{iss}"), 301
+  elsif query =~ %r{keyword=([^;]+)}
+    stuff = $1
+    redirect to("https://escholarship.org/search?q=#{stuff}"), 301
+  else
+    halt 401
+  end
+end
+
+###################################################################################################
+def handleBepressRedirect(request, query)
+  # Clean up the path a bit, then add the query
+  path = (request.path.sub(%{^//},'/').sub(%r{/+$},'')) + query
+  path.sub! '&amp;', '&'
+
+  # Ordering sometimes nonstandard
+  path.sub! %r{^/cgi/viewcontent.cgi\?article=([^&]+)&context=(.*)}, '/cgi/viewcontent.cgi?context=\2&article=\1'
+  path.sub! %r{^/context/(.*)/article/(.*)/type/pdf/viewcontent}, '/cgi/viewcontent.cgi?context=\1&article=\2'
+
+  # Remove more and more of the path, looking for an article or ORU match
+  while path.length > 1
+    puts "attempt #{path}"
+    if (redir = Redirect.where(kind: "bepress", from_path: path).first)
+      redirect to("https://escholarship.org#{redir.to_path}"), 301
+      return
+    end
+    path.sub!(%r{/[^/]+$},'')
+  end
+
+  # Fallback - see if there's any context that would lead to a unit
+  if (request.path + query) =~ %r{context[=/]([^/&]=)}
+    unit = $1
+    redirect to("https://escholarship.org#{$unitsHash[unit] ? "/#{unit}" : ""}"), 301
+  else
+    redirect to("https://escholarship.org"), 301
+  end
+end
+
+###################################################################################################
+def handleDojRedirect(request, query)
+  path = request.path
+  variations = [path,
+                "#{path}/index.html",
+                "#{path}/index.htm",
+                path.sub(%r{/[^/]+$}, "/_catch_all"),
+                path.sub(%r{/[^/]+/[^/]+$}, "/_catch_all"),
+                path.sub(%r{/[^/]+/[^/]+/[^/]+$}, "/_catch_all")]
+  variations.each { |var|
+    if (redir = Redirect.where(kind: "doj", from_path: var).first)
+      redirect to("https://escholarship.org#{redir.to_path}"), 301
+    end
+  }
+  redirect to("https://escholarship.org/uc/doj"), 301
 end
 
 ###################################################################################################
@@ -344,11 +433,13 @@ get "/content/:fullItemID/*" do |itemID, path|
   # Control how long this remains in browser and CloudFront caches
   cache_control :public, :max_age => 3600   # maybe more?
 
-  # Allow cross-origin requests so that main site and CloudFront cache can co-operate
+  # Allow cross-origin requests so that main site and CloudFront cache can co-operate. Also, let
+  # crawlers know that the canonical URL is the item page.
   headers "Access-Control-Allow-Origin" => "*",
           "Access-Control-Allow-Headers" => "Range",
           "Access-Control-Expose-Headers" => "Accept-Ranges, Content-Encoding, Content-Length, Content-Range",
-          "Access-Control-Allow-Methods" => "GET, OPTIONS"
+          "Access-Control-Allow-Methods" => "GET, OPTIONS",
+          "Link" => "<https://escholarship.org/uc/item/#{itemID.sub(/^qt/,'')}>; rel=\"canonical\""
 
   # Stream supp files out directly from Merritt. Also, if there's no display PDF, fall back
   # to the version in Merritt.
--- tools/testRedirects.rb
+++ tools/testRedirects.rb
@@ -12,37 +12,106 @@ def testRedirect(fromURL, toURL)
   host = uri.host
   cmd = %{curl --silent --resolve #{host}:4001:127.0.0.1 -I "#{fromURL.sub(/:\/\/([^\/]+)/, '://\1:4001')}"}
   result = `#{cmd}`.encode!('UTF-8', 'UTF-8', :invalid => :replace)
-  if result =~ %r{HTTP/1.1 (30[12]).*Location: ([^\n]+)}m
+  if result =~ %r{HTTP/1.1 (30[123]).*Location: ([^\n]+)}m
     got = $2.strip
-    toURL == got or error("Incorrect redirect. #{fromURL} -> #{got} but expected #{toURL}")
+    toURL == got or error("Incorrect redirect. #{fromURL.inspect} -> #{got.inspect} but expected #{toURL.inspect}")
   elsif result =~ %r{HTTP/1.1 (\d+)}
     code = $1.to_i
     if code == 200
-      error "#{fromURL} got 200, should have redirected to #{toURL}"
+      return if toURL.nil?
+      error "#{fromURL.inspect} got 200, should have redirected to #{toURL.inspect}"
     else
-      error "error #{$1} from #{fromURL}"
+      error "error #{$1} from #{fromURL.inspect}"
     end
   else
-    error "huh? #{fromURL} #{result}"
+    puts cmd
+    error "huh? #{fromURL.inspect} #{result.inspect}"
   end
 end
 
+# DOJ redirects
+testRedirect("http://dermatology.cdlib.org//143/index.html",
+             "https://escholarship.org/uc/doj/14/3")
+testRedirect("http://dermatology.cdlib.org//102/case_presentations/pseudomonas/2.jpg",
+             "https://escholarship.org/uc/item/6fz6c3r8")
+testRedirect("http://dermatology.cdlib.org//search?uri=1&go=1&entity=doj&keyword=Search%20journal...&q=1&scope=../../../../../../../../../../../../../../../../etc/passwd",
+             "https://escholarship.org/uc/doj")
+
+# Bepress redirects
+testRedirect("http://repositories.cdlib.org/uctc",
+             "https://escholarship.org/uc/uctc")
+testRedirect("http://repositories.cdlib.org/its/tsc",
+             "https://escholarship.org/uc/its_tsc")
+testRedirect("http://repositories.cdlib.org/acgcc/jtas/vol1/iss1/art8",
+             "https://escholarship.org/uc/item/4035s7ss")
+testRedirect("http://repositories.cdlib.org//acgcc/jtas/emoryelliott.html",
+             "https://escholarship.org/uc/acgcc_jtas")
+testRedirect("http://repositories.cdlib.org/cgi/viewcontent.cgi?article=1006&context=its/tsc",
+             "https://escholarship.org/uc/item/0201j0v2")
+testRedirect("http://repositories.cdlib.org/context/tc/article/1194/type/pdf/viewcontent",
+             "https://escholarship.org/uc/item/8hk6960q")
+testRedirect("http://repositories.cdlib.org//cgi/viewcontent.cgi?context=its/tsc&amp;article=1045",
+             "https://escholarship.org/uc/item/1h52s226")
+
+# Programmed unit redirects from unitRedirect.xsl
+testRedirect("http://escholarship.org/uc/ioe",
+             "https://escholarship.org/uc/ioes")
+
+# Unit to landing page
+testRedirect("http://escholarship.org/uc/search?entity=uciem_westjem",
+             "https://escholarship.org/uc/uciem_westjem")
+
+# Journal issue
+testRedirect("http://escholarship.org/uc/search?entity=uciem_westjem;volume=18;issue=6.1",
+             "https://escholarship.org/uc/uciem_westjem/18/6.1")
+
+# Keyword search
+testRedirect("http://escholarship.org/uc/search?keyword=japan",
+             "https://escholarship.org/search?q=japan")
+
+# Redirect eScholarship Editions
+testRedirect("http://escholarship.org/editions/view?docId=ft6d5nb46p&chunk.id=nsd0e372&toc.id=endnotes&toc.depth=1&brand=ucpress&anchor.id=d0e436",
+             "https://publishing.cdlib.org/ucpressebooks/view?docId=ft6d5nb46p&chunk.id=nsd0e372&toc.id=endnotes&toc.depth=1&brand=ucpress&anchor.id=d0e436")
+
+# Redirect garbled or old searches
+testRedirect("http://escholarship.org/uc/item/3nm3m1rv?query=%E5%98%97%E9%BA%A5&hitNum=1&scroll=yes",
+             "https://escholarship.org/uc/item/3nm3m1rv")
+
+# Redirect pvw item to main item
+testRedirect("http://pvw.escholarship.org/uc/item/5j20j32b",
+             "https://escholarship.org/uc/item/5j20j32b")
+
+# Redirect eprints to eschol
+testRedirect("http://eprints.cdlib.org/uc/item/8gj3x1dc",
+             "https://escholarship.org/uc/item/8gj3x1dc")
+
+# Redirect eschol.cdlib
+testRedirect("http://escholarship.cdlib.org/uc/item/9ws876kn.pdf",
+             "https://escholarship.org/uc/item/9ws876kn.pdf")
+
+# Redirect old PDF links
+testRedirect("http://escholarship.org/uc/item/4590m805.pdf",
+             "https://cloudfront.escholarship.org/dist/prd/content/qt4590m805/qt4590m805.pdf")
+
+# No longer redirecting PDF links
+testRedirect("http://escholarship.org/content/qt4590m805/qt4590m805.pdf", nil)
+
 # Redirect from supp file to item
 testRedirect("http://escholarship.org/uc/item/3kq9770m/socr_pt4_20070806_v256_part_2.mpg",
-             "http://escholarship.org/uc/item/3kq9770m")
+             "https://escholarship.org/uc/item/3kq9770m")
 
 # Strip query parameters from item URLs
 testRedirect("http://escholarship.org/uc/item/0nm3g51w?foo=bar",
-             "http://escholarship.org/uc/item/0nm3g51w")
+             "https://escholarship.org/uc/item/0nm3g51w")
 
 # Specific item to item redirects
 testRedirect("http://escholarship.org/uc/item/0fz9h5rt",
-             "http://escholarship.org/uc/item/13x9v6ms")
+             "https://escholarship.org/uc/item/13x9v6ms")
 
 # Redirect www.escholarship to escholarship
 testRedirect("http://www.escholarship.org",
-             "http://escholarship.org/")
+             "https://escholarship.org/")
 testRedirect("http://www.escholarship.org/uc/uclta",
-             "http://escholarship.org/uc/uclta")
+             "https://escholarship.org/uc/uclta")
 testRedirect("http://www.escholarship.org/uc/uclta?foo=bar",
-             "http://escholarship.org/uc/uclta?foo=bar")
+             "https://escholarship.org/uc/uclta?foo=bar")
