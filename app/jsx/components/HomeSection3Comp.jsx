fb958317a7547e4ed7b4637f8f4ca4822e569fc4
Andy Mardesich <andy.mardesich@ucop.edu>
2017-10-17 14:17:00 -0700

    Joel's latest changes

--- app/jsx/components/HomeSection3Comp.jsx
+++ app/jsx/components/HomeSection3Comp.jsx
@@ -7,17 +7,17 @@ import { Link } from 'react-router'
 class HomeSection3Comp extends React.Component {
   render() {
     return (
-      <div id="publishing" className="c-homesection3">
-        <div className="c-homesection3__description">
+      <div id="publishing" className="c-homesection__3">
+        <div className="c-homesection__3-description">
           <h3>eScholarship Publishing</h3>
           <p>eScholarship Publishing is an open access publishing platform subsidized by the University of California, managed by the California Digital Library, and offered free of charge to UC-affiliated departments, research units, publishing programs and individual scholars.</p>
           <p>We offer publishing and production tools, including a full editorial and peer review system, as well as professional support and consulting services.</p>
         </div>
-        <div className="c-homesection3__journalgrid">
+        <div className="c-homesection__3-journalgrid">
           <JournalGridComp />
         </div>
-        <Link to="/journals" className="c-homesection3__browse-all">Browse all eScholarship journals</Link>
-        <a href="https://help.escholarship.org/support/solutions/articles/9000127857-starting-a-new-journal-on-escholarship" className="c-homesection3__start">How to start a journal</a>
+        <Link to="/journals" className="c-homesection__3-browse-all">Browse all eScholarship journals</Link>
+        <a href="https://help.escholarship.org/support/solutions/articles/9000127857-starting-a-new-journal-on-escholarship" className="c-homesection__3-start">How to start a journal</a>
       </div>
     )
   }
