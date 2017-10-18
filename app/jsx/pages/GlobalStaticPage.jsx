ddfe812e5d7c218bca19447c1578a3dc7ed9e8c8
Andy Mardesich <andy.mardesich@ucop.edu>
2017-10-18 15:41:49 -0700

    Deposit button working on static pages again

--- app/jsx/pages/GlobalStaticPage.jsx
+++ app/jsx/pages/GlobalStaticPage.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { Link } from 'react-router'
 
 import PageBase from './PageBase.jsx'
-import Header1Comp from '../components/Header1Comp.jsx'
-import NavComp from '../components/NavComp.jsx'
-import FooterComp from '../components/FooterComp.jsx'
 import ServerErrorComp from '../components/ServerErrorComp.jsx'
-import UnitStaticPageLayout from '../layouts/UnitStaticPageLayout.jsx'
+import Header1Comp from '../components/Header1Comp.jsx'
 import NavBarComp from '../components/NavBarComp.jsx'
-import SidebarComp from '../components/SidebarComp.jsx'
 import BreadcrumbComp from '../components/BreadcrumbComp.jsx'
+import WizardComp from '../components/WizardComp.jsx'
+import SidebarComp from '../components/SidebarComp.jsx'
+import UnitStaticPageLayout from '../layouts/UnitStaticPageLayout.jsx'
 
 class NotFoundLayout extends React.Component
 {
@@ -25,6 +24,12 @@ class NotFoundLayout extends React.Component
 
 export default class GlobalStaticPage extends PageBase
 {
+  state = { modalOpen: false }
+
+  closeWizardModal = e => {
+    this.setState({modalOpen:false})
+  }
+
   // PageBase will fetch the following URL for us, and place the results in this.state.pageData
   pageDataURL() {
     return `/api/globalStatic/${this.props.params.splat}`
@@ -39,12 +44,21 @@ export default class GlobalStaticPage extends PageBase
     let sidebar = <SidebarComp data={data.sidebar}/>
     return(
       <div>
-        <Header1Comp campuses={data.header.campuses} />
+        <Header1Comp/>
         <NavBarComp navBar={data.header.nav_bar} unit={data.unit} socialProps={data.header.social} />
         { data.pageNotFound
           ? <NotFoundLayout/>
-          : <div>
+          : <div id="wizardModalBase">
               <BreadcrumbComp array={data.header.breadcrumb} />
+              <button style={{display: 'none'}} id="wizardlyDeposit" onClick={(event)=>{
+                  this.setState({modalOpen:true})
+                  event.preventDefault()} } >Deposit</button>
+              <WizardComp showModal={this.state.modalOpen}
+                  parentSelector={()=>$('#wizardModalBase')[0]}
+                  onCancel={e=>this.closeWizardModal(e)}
+                  campuses={data.header.campuses}
+                  data={{campusID: null, campusName: null, unitID: null, unitName: null}}
+                />
               <UnitStaticPageLayout unit={data.unit} data={data.content} sidebar={sidebar} fetchPageData={this.fetchPageData}/>
             </div>
         }
