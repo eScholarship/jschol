f7f89a0978d49b10b969925d0e32e36e2083c96d
Martin Haye <martin.haye@ucop.edu>
2017-10-23 09:19:41 -0700

    Better facebook image, hopefully.

--- app/jsx/components/MetaTagsComp.jsx
+++ app/jsx/components/MetaTagsComp.jsx
@@ -26,7 +26,9 @@ export default class MetaTagsComp extends React.Component
         <meta id="og-title" property="og:title" content={finalTitle} />
         { this.props.descrip && <meta id="meta-description" name="description" content={this.stripHtml(this.props.descrip)} /> }
         { this.props.descrip && <meta id="og-description" name="og:description" content={this.stripHtml(this.props.descrip)} /> }
-        <meta id="og-image" property="og:image" content="https://escholarship.org/images/escholarship-facebook.jpg" />
+        <meta id="og-image" property="og:image" content="https://escholarship.org/images/escholarship-facebook2.jpg" />
+        <meta id="og-image-width" property="og:image:width" content="1242" />
+        <meta id="og-image-height" property="og:image:height" content="1242" />
         { this.props.children }
       </MetaTags>
     )
