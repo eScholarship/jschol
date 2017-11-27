4ce169539962cfb4e26c491a58b57f0b556f684f
Andy Mardesich <andy.mardesich@ucop.edu>
2017-11-27 12:16:06 -0800

    Published web location (or DOI) on item view

--- app/jsx/components/PubLocationComp.jsx
+++ app/jsx/components/PubLocationComp.jsx
@@ -4,14 +4,18 @@ import React from 'react'
 import RightsComp from '../components/RightsComp.jsx'
 
 class PubLocationComp extends React.Component {
+  // Display DOI or pub_web_loc. If both DOI and pub_web_loc provided, display pub_web_loc
   render() {
-    let pub_loc_block = null
-    if (this.props.pub_web_loc && this.props.pub_web_loc.length > 0) {
-      pub_loc_block = 
+    let doi
+    if (this.props.doi) { doi = this.props.doi.startsWith("http") ? this.props.doi : "https://doi.org/" + this.props.doi }
+    let pub_loc_block = (this.props.pub_web_loc && this.props.pub_web_loc.length > 0) ?
         this.props.pub_web_loc.map(function(url, i) {
           return ( <a key={i} className="c-publocation__link" href={url}>{url}</a> )
         })
-    }
+      :
+      doi ?
+        <a href={doi} className="c-publocation__link">{doi}</a>
+        : null
     return (
       <div className="c-publocation">
         {pub_loc_block &&
