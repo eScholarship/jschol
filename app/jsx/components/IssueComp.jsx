3c4c94630422705a7704a7a651e27faf611a279f
Andy Mardesich <andy.mardesich@ucop.edu>
2017-12-06 11:13:04 -0800

    Issue caption temporary truncation

--- app/jsx/components/IssueComp.jsx
+++ app/jsx/components/IssueComp.jsx
@@ -5,6 +5,11 @@ import React from 'react'
 class IssueComp extends React.Component {
   render() {
     let p = this.props
+    let caption
+    if (p.cover && p.cover.caption && p.cover.caption.length > 58) {
+      caption = p.cover.caption.substring(0, 58)
+      caption = (p.cover.caption.length > 58) ? caption + "..." : caption
+    }
     return (
       <div className="c-issue">
       {p.title &&
@@ -13,8 +18,8 @@ class IssueComp extends React.Component {
       {p.cover &&
         <figure className="c-issue__thumbnail">
           <img src={"/assets/"+p.cover.asset_id} alt="Issue cover" />
-        {p.cover.caption &&
-          <figcaption><i>Cover Caption:</i> {p.cover.caption}</figcaption>
+        {caption &&
+          <figcaption><i>Cover Caption:</i> {caption}</figcaption>
         }
         </figure>
       }
