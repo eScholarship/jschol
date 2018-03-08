6fbd5b062c073ffe87de27d3653493559b6ba16c
Martin Haye <martin.haye@ucop.edu>
2018-03-08 12:46:16 -0800

    Changed updated from Date to DateTime, to support finer-grained API querying.

--- migrations/0028_itemSorting.rb
+++ migrations/0028_itemSorting.rb
@@ -3,7 +3,7 @@ Sequel.migration do
     alter_table(:items) do
       rename_column :pub_date, :published
       rename_column :eschol_date, :added
-      add_column :updated, Date
+      add_column :updated, DateTime
       add_column :submitted, Date
       add_index [:status, :published, :id]
       add_index [:status, :added, :id]
