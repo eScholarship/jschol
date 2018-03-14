64cfe4c152f8a4d4f65567788d41e0f469a38699
Martin Haye <martin.haye@ucop.edu>
2018-03-14 15:27:15 -0700

    Last of the integrations for objects; these are all ignored in jschol anyway.

--- app/jsx/objects/DivideContentObj.jsx
+++ app/jsx/objects/DivideContentObj.jsx
@@ -1,29 +1,24 @@
 // ##### Divide Content Objects ##### //
 
 import React from 'react'
+import faker from 'faker/locale/en'
 
 class DivideContentObj extends React.Component {
   render() {
     return (
       <div>
-      <h1>Divide Content into 2 Balanced Columns</h1>
+        <h1>Divide Content into 2 Balanced Columns</h1>
         <div className="o-dividecontent2x">
           <img src="http://placehold.it/250x150?text=Image" alt="" />
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias quaerat consectetur sint? Veritatis quis, iure consectetur eius neque ad facilis, reiciendis itaque tempora labore et exercitationem quidem? Eius quibusdam numquam illum aliquam illo! Nesciunt optio minus aspernatur quibusdam sequi quidem odio tempore, nulla pariatur est debitis ipsam labore, laboriosam, eveniet.
-          </p>
-          <p>Labore quibusdam cumque nulla quas, asperiores nobis mollitia odio necessitatibus quae perferendis! Accusamus dolorum qui ipsum, illum. Ab quae, dicta dolore. Asperiores corporis expedita rem inventore, aperiam ducimus adipisci repellat, quia saepe quaerat perferendis vitae minus voluptatum accusamus libero aspernatur sed. Error dolorem a repellendus fuga facere nemo non architecto.
-          </p>
-          <p>Velit accusamus at maxime vero ad dolore sapiente, commodi tenetur, repudiandae non esse delectus neque nulla quibusdam fugit voluptatibus error cupiditate blanditiis. Nemo similique, accusamus. Pariatur, sint fuga odit reprehenderit quo obcaecati voluptatem nobis impedit laudantium quisquam praesentium, cupiditate tenetur at atque, ratione quibusdam alias perspiciatis. Nobis, ipsum voluptate aliquam?
-          </p>
+          <p>{faker.fake("{{lorem.paragraphs}}")}</p>
+          <p>{faker.fake("{{lorem.paragraphs}}")}</p>
+          <p>{faker.fake("{{lorem.paragraphs}}")}</p>
         </div>
         <h1>Divide Content into 2 Balanced Columns with Rule</h1>
         <div className="o-dividecontent2x--ruled">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vel reiciendis, aut perferendis eius repudiandae modi quibusdam quasi recusandae, ad nobis culpa suscipit magnam voluptatibus ex, minus repellendus est maxime consequuntur aperiam. Earum delectus, aspernatur molestias ipsam ipsum qui esse eaque voluptas. Ea adipisci, eius, repellendus itaque laudantium officia eligendi!
-          </p>
-          <p>Repudiandae corporis perspiciatis modi deleniti sapiente, perferendis aspernatur ullam, earum minima ducimus tenetur suscipit quis veniam. Id aliquam, nemo est recusandae aspernatur perspiciatis dignissimos? Nam cumque repellendus enim tempore repellat qui, a molestias ratione eos cum, maxime hic similique ipsa ea aut temporibus sunt assumenda, esse obcaecati illo! Officia, sed.
-          </p>
-          <p>Voluptas sed impedit, itaque cum ratione, aliquam molestias repellat. Cupiditate vitae corporis nulla doloremque? Quibusdam asperiores adipisci, recusandae rerum debitis quaerat repellendus architecto, repudiandae in fugiat, nemo nostrum tempore laborum. Sapiente, iusto, vero. Minima sapiente porro nemo, cum iste optio doloremque molestias eius sunt unde eligendi officia ad eos voluptate.
-          </p>
+          <p>{faker.fake("{{lorem.paragraphs}}")}</p>
+          <p>{faker.fake("{{lorem.paragraphs}}")}</p>
+          <p>{faker.fake("{{lorem.paragraphs}}")}</p>
           <img src="http://placehold.it/250x150?text=Image" alt="" />
         </div>
       </div>
