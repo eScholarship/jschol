357df0484f37250c16095af9ac618f925ab6920f
Andy Mardesich <andy.mardesich@ucop.edu>
2018-03-29 11:24:47 -0700

    Editor and Advisor names added to AuthorList

--- app/jsx/components/AuthorListComp.jsx
+++ app/jsx/components/AuthorListComp.jsx
@@ -23,32 +23,21 @@ class AuthorListComp extends React.Component {
   render() {
     let p = this.props,
         year = p.pubdate.match(/\d{4}/),
-        t_authornames = p.authors ? this.titledNames("Author", p.authors) : "",
-        t_editornames = p.editors ? this.titledNames("Editor", p.editors) : "",
-        t_advisornames = p.advisors ? this.titledNames("Advisor", p.advisors) : "",
+        t_authornames = (p.authors && !p.author_hide) ? this.titledNames("Author", p.authors) : [""],
+        t_editornames = p.editors ? this.titledNames("\xa0\xa0Editor", p.editors) : [""],
+        t_advisornames = p.advisors ? this.titledNames("\xa0\xa0Advisor", p.advisors) : [""],
         combined = t_authornames.concat(t_editornames).concat(t_advisornames).filter(e => typeof e === 'string' && e !== ''),
-        authorlist = p.authors.map((author, i) => { return (<li key={i+author.name}>{author.name}</li>) }),
-        editorlist = p.editors.map((editor, i) => { return (<li key={i+editor.name}>{editor.name}</li>) })
+        authorlist = combined.map((name, i) => { return (<li key={i+name}>{name}</li>) })
     return (
       <div className="c-authorlist">
         {year && <time className="c-authorlist__year">{year[0]}</time> }
         {/* The 'dangerouslySetInnerHTML' rigarmarole below is to keep React from getting upset about
             jquery.dotdotdot fiddling around with the author elements. */}
-        { !p.author_hide && p.authors &&
+        { combined && combined.length > 0 &&
           <TruncationObj element="ul" className="c-authorlist__list"
                          options={{watch:'window', after:'.c-authorlist__list-more-link', ellipsis:' ', wrap:'children',
                                    callback: () => $('.c-authorlist__list-more-link').click((e)=>this.handleClick(e, 'author'))}}>
             {authorlist}
-            {/* Note: the <a> more-link below cannot be a <Link>, else jquery dotdotdot can't recognize it */}
-            <li><a href="" className="c-authorlist__list-more-link">et al.</a></li>
-          </TruncationObj>
-        }
-        { p.editors &&
-          <TruncationObj element="ul" className="c-authorlist__list"
-                         options={{watch:'window', after:'.c-authorlist__list-more-link', ellipsis:' ', wrap:'children',
-                                   callback: () => $('.c-authorlist__list-more-link').click((e)=>this.handleClick(e, 'author'))}}>
-            <li>Editors: </li>
-            {editorlist}
             {/* Note: the <a> more-link below cannot be a <Link>, else jquery dotdotdot can't recognize it */}
             <li><a href="" className="c-authorlist__list-more-link">et al.</a></li>
           </TruncationObj>
