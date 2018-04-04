246857a1777005917c81a04260fe954a4a3d2017
Andy Mardesich <andy.mardesich@ucop.edu>
2018-04-04 12:15:10 -0700

    Integrated authorlist: Added spacing and bolding to Editors and Advisors

--- app/jsx/components/AuthorListComp.jsx
+++ app/jsx/components/AuthorListComp.jsx
@@ -14,30 +14,37 @@ class AuthorListComp extends React.Component {
   }
 
   // Expects an array containing hashes with a 'name' attribute [{name: "Stone, Elizabeth C.", }]
-  // Returns array of just names, with first name prepended with a title
-  titledNames = (title, array) => {
+  // Returns list elements of just names, with first name prepended with a title
+  asList = (title, array) => {
     return array.map((x, i) => {
-      return (i==0) ? `${title}(s): ` + x.name : x.name })
+      let c = (i==0) ? "c-authorlist__begin" : (i+1 == array.length) ? "c-authorlist__end" : null
+      if (i==0) {
+        return (<li key={i+x.name} className={c}><span className="c-authorlist__heading">{title}(s):</span> {x.name}</li>)
+      } else {
+        return (<li key={i+x.name} className={c}>{x.name}</li>)
+      }
+    })
   }
 
   render() {
     let p = this.props,
         year = p.pubdate.match(/\d{4}/),
-        t_authornames = (p.authors && !p.author_hide) ? this.titledNames("Author", p.authors) : [""],
-        t_editornames = p.editors ? this.titledNames("\xa0\xa0Editor", p.editors) : [""],
-        t_advisornames = p.advisors ? this.titledNames("\xa0\xa0Advisor", p.advisors) : [""],
-        combined = t_authornames.concat(t_editornames).concat(t_advisornames).filter(e => typeof e === 'string' && e !== ''),
-        authorlist = combined.map((name, i) => { return (<li key={i+name}>{name}</li>) })
+        authors = (p.authors && !p.author_hide) ? this.asList("Author", p.authors) : null,
+        editors = p.editors ? this.asList("Editor", p.editors) : null,
+        advisors = p.advisors ? this.asList("Advisor", p.advisors) : null
     return (
       <div className="c-authorlist">
         {year && <time className="c-authorlist__year">{year[0]}</time> }
         {/* The 'dangerouslySetInnerHTML' rigarmarole below is to keep React from getting upset about
             jquery.dotdotdot fiddling around with the author elements. */}
-        { combined && combined.length > 0 &&
+        { ((authors && authors.length > 0) || (editors && editors.length > 0) ||
+           (advisors && advisors.length > 0)) &&
           <TruncationObj element="ul" className="c-authorlist__list"
                          options={{watch:'window', after:'.c-authorlist__list-more-link', ellipsis:' ', wrap:'children',
                                    callback: () => $('.c-authorlist__list-more-link').click((e)=>this.handleClick(e, 'author'))}}>
-            {authorlist}
+            {authors}
+            {editors}
+            {advisors}
             {/* Note: the <a> more-link below cannot be a <Link>, else jquery dotdotdot can't recognize it */}
             <li><a href="" className="c-authorlist__list-more-link">et al.</a></li>
           </TruncationObj>
