a71b396ef1ea4603ed466bab6467822e8d429457
Lisa Schiff <lisa.schiff@ucop.edu>
2018-12-04 09:59:34 -0800

    Made tracker name unique to refract

--- app/jsx/pages/PageBase.jsx
+++ app/jsx/pages/PageBase.jsx
@@ -205,7 +205,7 @@ class PageBase extends React.Component
       if (/^nanocad/.test(unit_id)) { this.runExtGATracker('nanocadTracker', 'UA-17962781-1') }
       if (/^uciem_westjem/.test(unit_id)) { this.runExtGATracker('westjemTracker', 'UA-34762732-1') }
       if (/^ucla_epss/.test(unit_id)) { this.runExtGATracker('epssTracker', 'UA-111990925-2') }
-      if (/^refract/.test(unit_id)) { this.runExtGATracker('epssTracker', 'UA-130336975-1') }      
+      if (/^refract/.test(unit_id)) { this.runExtGATracker('refractTracker', 'UA-130336975-1') }      
     }
   }
 
