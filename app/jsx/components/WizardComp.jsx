63aefc59d68f89a3ea57c997ac5242544addc6a0
Martin Haye <martin.haye@ucop.edu>
2019-03-01 16:52:47 -0800

    Fixed indentation.

--- app/jsx/components/WizardComp.jsx
+++ app/jsx/components/WizardComp.jsx
@@ -134,57 +134,57 @@ class WizardComp extends React.Component {
     let d = this.state.data
     return (
       <div className="c-modal">
-      <Contexts.Wiz.Provider value={
-         { launchedFromRoot: this.state.launchedFromRoot,
-           campusName: d.campusName ? d.campusName : "eScholarship", 
-           campusID: d.campusID,
-           unitID: d.unitID,
-           unitName: d.unitName,
-           seriesName: d.seriesName,
-           arg: d.arg,
-           type: d.type
-      } }>
+        <Contexts.Wiz.Provider value={
+           { launchedFromRoot: this.state.launchedFromRoot,
+             campusName: d.campusName ? d.campusName : "eScholarship",
+             campusID: d.campusID,
+             unitID: d.unitID,
+             unitName: d.unitName,
+             seriesName: d.seriesName,
+             arg: d.arg,
+             type: d.type
+        } }>
 
-        <WizModal isOpen={this.state.showModal && d.wizardStep === 1} onRequestClose={this.handleCloseModal} parentSelector={this.props.parentSelector}>
-          <div className={d.wizardStep === 1 ? `c-wizard__current-${d.wizardDir}` : `c-wizard__standby-${d.wizardDir}`} aria-hidden={d.wizardStep === 1 ? null : true}>
-            {/* [1] How are you affiliated with [campus]? */}
-            <WizardRoleComp goForward = {this.goForward} closeModal={this.handleCloseModal} />
-          </div>
-        </WizModal>
-        <WizModal isOpen={this.state.showModal && d.wizardStep === 2} onRequestClose={this.handleCloseModal} parentSelector={this.props.parentSelector}>
-          <div className={d.wizardStep === 2 ? `c-wizard__current-${d.wizardDir}` : `c-wizard__standby-${d.wizardDir}`} aria-hidden={d.wizardStep === 2 ? null : true}>
-            {/* [2] Which UC Campus are you affiliated with? */}
-            <WizardCampusComp goForward = {this.goForward} goBackward = {this.goBackward} closeModal={this.handleCloseModal}
-                            campuses={d.campuses} subi_link = {SUBI_LINK} />
-          </div>
-        </WizModal>
-        <WizModal isOpen={this.state.showModal && d.wizardStep === 3} onRequestClose={this.handleCloseModal} parentSelector={this.props.parentSelector}>
-          <div className={d.wizardStep === 3 ? `c-wizard__current-${d.wizardDir}` : `c-wizard__standby-${d.wizardDir}`} aria-hidden={d.wizardStep === 3 ? null : true}>
-            {/* [3] What kind of material are you depositing? */}
-            <WizardTypeComp goForward = {this.goForward} goBackward = {this.goBackward} closeModal={this.handleCloseModal}
-                            subi_link = {SUBI_LINK} />
-          </div>
-        </WizModal>
-        <WizModal isOpen={this.state.showModal && d.wizardStep === 4} onRequestClose={this.handleCloseModal} parentSelector={this.props.parentSelector}>
-          <div className={d.wizardStep === 4 ? `c-wizard__current-${d.wizardDir}` : `c-wizard__standby-${d.wizardDir}`} aria-hidden={d.wizardStep === 4 ? null : true}>
-            {/* [4] What is your departmental affiliation? */}
-            <WizardUnitComp goForward = {this.goForward} goBackward = {this.goBackward} closeModal={this.handleCloseModal} />
-          </div>
-        </WizModal>
-        <WizModal isOpen={this.state.showModal && d.wizardStep === 5} onRequestClose={this.handleCloseModal} parentSelector={this.props.parentSelector}>
-          <div className={d.wizardStep === 5 ? `c-wizard__current-${d.wizardDir}` : `c-wizard__standby-${d.wizardDir}`} aria-hidden={d.wizardStep === 5 ? null : true}>
-            {/* [5] What [title] series would you like to deposit your work in?  */}
-            <WizardSeriesComp goForward = {this.goForward} goBackward = {this.goBackward} closeModal={this.handleCloseModal}
+          <WizModal isOpen={this.state.showModal && d.wizardStep === 1} onRequestClose={this.handleCloseModal} parentSelector={this.props.parentSelector}>
+            <div className={d.wizardStep === 1 ? `c-wizard__current-${d.wizardDir}` : `c-wizard__standby-${d.wizardDir}`} aria-hidden={d.wizardStep === 1 ? null : true}>
+              {/* [1] How are you affiliated with [campus]? */}
+              <WizardRoleComp goForward = {this.goForward} closeModal={this.handleCloseModal} />
+            </div>
+          </WizModal>
+          <WizModal isOpen={this.state.showModal && d.wizardStep === 2} onRequestClose={this.handleCloseModal} parentSelector={this.props.parentSelector}>
+            <div className={d.wizardStep === 2 ? `c-wizard__current-${d.wizardDir}` : `c-wizard__standby-${d.wizardDir}`} aria-hidden={d.wizardStep === 2 ? null : true}>
+              {/* [2] Which UC Campus are you affiliated with? */}
+              <WizardCampusComp goForward = {this.goForward} goBackward = {this.goBackward} closeModal={this.handleCloseModal}
+                              campuses={d.campuses} subi_link = {SUBI_LINK} />
+            </div>
+          </WizModal>
+          <WizModal isOpen={this.state.showModal && d.wizardStep === 3} onRequestClose={this.handleCloseModal} parentSelector={this.props.parentSelector}>
+            <div className={d.wizardStep === 3 ? `c-wizard__current-${d.wizardDir}` : `c-wizard__standby-${d.wizardDir}`} aria-hidden={d.wizardStep === 3 ? null : true}>
+              {/* [3] What kind of material are you depositing? */}
+              <WizardTypeComp goForward = {this.goForward} goBackward = {this.goBackward} closeModal={this.handleCloseModal}
                               subi_link = {SUBI_LINK} />
-          </div>
-        </WizModal>
-        <WizModal isOpen={this.state.showModal && d.wizardStep === 6} onRequestClose={this.handleCloseModal} parentSelector={this.props.parentSelector}>
-          <div className={d.wizardStep === 6 ? `c-wizard__current-${d.wizardDir}` : `c-wizard__standby-${d.wizardDir}`} aria-hidden={d.wizardStep === 6 ? null : true}>
-            {/* [6] (LinkModal: Screens vary by arg) */}
-            <WizardLinkComp goBackward = {this.goBackward} closeModal={this.handleCloseModal} />
-          </div>
-        </WizModal>
-      </Contexts.Wiz.Provider>
+            </div>
+          </WizModal>
+          <WizModal isOpen={this.state.showModal && d.wizardStep === 4} onRequestClose={this.handleCloseModal} parentSelector={this.props.parentSelector}>
+            <div className={d.wizardStep === 4 ? `c-wizard__current-${d.wizardDir}` : `c-wizard__standby-${d.wizardDir}`} aria-hidden={d.wizardStep === 4 ? null : true}>
+              {/* [4] What is your departmental affiliation? */}
+              <WizardUnitComp goForward = {this.goForward} goBackward = {this.goBackward} closeModal={this.handleCloseModal} />
+            </div>
+          </WizModal>
+          <WizModal isOpen={this.state.showModal && d.wizardStep === 5} onRequestClose={this.handleCloseModal} parentSelector={this.props.parentSelector}>
+            <div className={d.wizardStep === 5 ? `c-wizard__current-${d.wizardDir}` : `c-wizard__standby-${d.wizardDir}`} aria-hidden={d.wizardStep === 5 ? null : true}>
+              {/* [5] What [title] series would you like to deposit your work in?  */}
+              <WizardSeriesComp goForward = {this.goForward} goBackward = {this.goBackward} closeModal={this.handleCloseModal}
+                                subi_link = {SUBI_LINK} />
+            </div>
+          </WizModal>
+          <WizModal isOpen={this.state.showModal && d.wizardStep === 6} onRequestClose={this.handleCloseModal} parentSelector={this.props.parentSelector}>
+            <div className={d.wizardStep === 6 ? `c-wizard__current-${d.wizardDir}` : `c-wizard__standby-${d.wizardDir}`} aria-hidden={d.wizardStep === 6 ? null : true}>
+              {/* [6] (LinkModal: Screens vary by arg) */}
+              <WizardLinkComp goBackward = {this.goBackward} closeModal={this.handleCloseModal} />
+            </div>
+          </WizModal>
+        </Contexts.Wiz.Provider>
       </div>
     )
   }
