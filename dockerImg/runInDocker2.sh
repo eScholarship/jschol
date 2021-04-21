a0fc0a24a6bed684bcd028ec577ead9b14b92b4e
Hardy Pottinger <hardy.pottinger@gmail.com>
2021-04-21 14:31:02 -0500

    Remove old/unused Docker stuff

--- dockerImg/runInDocker2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd /home/jschol/inner_jschol
-mkdir -p dockerImg
-cp /outer_jschol/dockerImg/runInDocker3.sh dockerImg/
-cp /outer_jschol/config/env* config/
-dockerImg/runInDocker3.sh
--- runInDocker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Starting jschol in a Docker container."
-docker run -v "${PWD}:/outer_jschol" -p 4001:4001 -p 35729:35729 -it escholarship/jschol /outer_jschol/dockerImg/runInDocker2.sh
