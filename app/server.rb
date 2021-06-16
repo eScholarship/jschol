c402964c23574b8c8804b18d47ff59dde370146d
Brian Tingle <brian.tingle@ucop.edu>
2021-06-16 08:09:22 -0700

    clean up user input in passed around json

--- app/server.rb
+++ app/server.rb
@@ -629,7 +629,7 @@ def generalResponse
   # ReactRouter and React.
   response = HTTParty.post("http://#{$host}:#{ENV['ISO_PORT']}#{remainder}",
                :headers => { 'Content-Type' => 'application/json' },
-               :body => pageData.to_json,
+               :body => pageData.to_json.transform_values { |v| CGI::escapeHTML(v) } ,
                :timeout => 20)
   if response.code != 200
     # If there's an exception (like iso is completely dead), fall back to non-iso mode.
@@ -662,6 +662,8 @@ def generalResponse
   #
   # For a test sample, see the "5\%" near the end of the abstract in qt3f3256kv.
   jsonPageData.gsub!("\\", "\\\\\\\\")
+
+  jsonPageData.transform_values { |v| CGI::escapeHTML(v) }
 
   # In the template, substitute the results from React/ReactRouter, and the API data so the
   # client-side React code can successfully rehydrate the page.
