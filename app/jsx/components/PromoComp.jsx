41943e239c04784e09ab3123c77d79277ff15759
Hardy Pottinger <hardy.pottinger@gmail.com>
2022-04-26 16:35:47 -0500

    change href for link in promo bar, and have it open in a new window

--- app/jsx/components/PromoComp.jsx
+++ app/jsx/components/PromoComp.jsx
@@ -10,7 +10,7 @@ class PromoComp extends React.Component {
   render() {
     return (
       <div className={this.state.messageClose ? "c-promo--close" : "c-promo"} role="alert" hidden={this.state.messageClose ? true : false}>
-        <strong className="c-promo__main-text">eScholarship celebrates its 20-year anniversary! <a href="/celebrateeschol">Learn More &gt;</a></strong>
+        <strong className="c-promo__main-text">eScholarship celebrates its 20-year anniversary! <a href="https://osc.universityofcalifornia.edu/2022/04/escholarship-celebrates-its-20-year-anniversary/" target="_blank">Learn More &gt;</a></strong>
       </div>
     )
   }
