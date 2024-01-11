e41fca35f8d4b0a131d064fee0eda6310f7664a3
Mahjabeen Yucekul <mahjabeen.yucekul@ucop.edu>
2024-01-11 14:40:52 -0800

    Added comments

--- migrations/0035_unitidcascade.rb
+++ migrations/0035_unitidcascade.rb
@@ -1,3 +1,11 @@
+# The business requirement releated to the schema change is the need 
+# to update unit id. Unit id is used in a number of other tables and 
+# as a result of the foreign key constraint DB does not allow the unit id
+# change. The solution is to update the foreign key constraints to allow
+# for updated id to cascade to the other tables. 
+# Here is the documentation for the drop and add syntax to update the key 
+# constraints. 
+#https://github.com/jeremyevans/sequel/blob/master/doc/schema_modification.rdoc#add_foreign_key-
 Sequel.migration do
   up do
     # update foreign key constraints to cascade on unit id update
