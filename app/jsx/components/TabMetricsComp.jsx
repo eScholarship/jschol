0ff9b1b9f1c2779eba16b71449841c27c0cc58cb
EC2 Default User <ec2-user@ip-10-62-4-18.us-west-2.compute.internal>
2024-09-19 22:17:54 +0000

    Fixed typo in disclaimer

--- app/jsx/components/TabMetricsComp.jsx
+++ app/jsx/components/TabMetricsComp.jsx
@@ -187,7 +187,7 @@ class TabMetricsComp extends React.Component {
             <TotalUsage usage={this.props.usage} />
             <MonthlyUsage usage={this.props.usage} />
             <div className="o-well-colored">
-	     Note: Due to the evolving nature of the web traffic, the data presented here should be considered approximate and subject to revision.&nbsp;<a href="https://help.escholarship.org/support/solutions/folders/9000181583">Learn more</a>.
+	     Note: Due to the evolving nature of web traffic, the data presented here should be considered approximate and subject to revision.&nbsp;<a href="https://help.escholarship.org/support/solutions/folders/9000181583">Learn more</a>.
             </div>
           </div>
         :
