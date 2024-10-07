c2e1f38860d4ae0c66a9cdbbd9c4fa9c20ad5243
Martin Haye <martin.haye@ucop.edu>
2024-10-07 16:21:27 -0700

    Moved maybeSocks to util. Rev'd log formatter for new Rack

--- setup.sh
+++ setup.sh
@@ -5,19 +5,18 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # uncomment for debug
-#set -x
+set -x
 
 printf "== Installing local Ruby gems ==\n"
-gem install bundler
-bundle install --quiet --path=gems --binstubs
-
-#printf "\n== Uninstalling Ruby gems no longer used ==\n"
-bundle clean
+rm -rf gems bin Gemfile.lock
+mkdir bin
+bundle config --local set path gems
+bundle install
+bundle binstubs --all
 
 printf "\n== Installing node packages (used by gulp and iso via Node) ==\n"
-npm install npm@10.8.1
+rm -rf node-modules
 npm install
-npm install gulp-cli # shouldn't be necessary, but seems to be
 
 # add a symlink to node_modules/.bin/gulp in the root directory, if necessary
 if [ ! -L ./gulp ]; then
---
+++ util/maybeSocks.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+
+verbose = ARGV.delete('-v') || ARGV.delete('--verbose')
+
+# If there's no socks config, definitely don't start socks.
+if !ENV['SOCKS_PORT']
+  verbose and puts "No SOCKS proxy configured, so not starting one."
+  exit(0)
+end
+
+# See if there's already a proxy listening on the specified port
+port = ENV['SOCKS_PORT'].to_i
+begin
+  s = TCPSocket.open("127.0.0.1", port)
+  s.close()
+  verbose and puts "SOCKS proxy already running on port #{port}."
+  exit(0)
+rescue
+  # ok, good, we can't connect
+end
+
+targetMachine = ENV['SOCKS_TARGET'] || raise("missing env SOCKS_TARGET")
+
+bastion = ENV['SOCKS_BASTION']
+bastionPort = ENV['SOCKS_BASTION_PORT']
+
+# Fire up a proxy, optionally overriding username
+puts "\nStarting SOCKS proxy."
+user = ENV['SOCKS_USER'] ? "#{ENV['SOCKS_USER']}@" : ""
+keypath = ENV['SOCKS_KEYPATH'] ? "-i #{ENV['SOCKS_KEYPATH']} " : ""
+cmd = "ssh -4 #{keypath}-N -D #{port} " +
+      "-F /dev/null " +
+      "-o ServerAliveInterval=60 " +
+      "-o ServerAliveCountMax=30 " +
+      "#{bastion ? "-o ProxyCommand='ssh -C -W %h:%p -o ServerAliveInterval=60 -o ServerAliveCountMax=30 -o StrictHostKeyChecking=no -o CheckHostIP=no #{bastionPort ? "-p #{bastionPort}" : ""} #{user}bastion2.cdlib.org' " : ""}" +
+      "-o StrictHostKeyChecking=no " +
+      "-o CheckHostIP=no " +
+      "#{user}#{targetMachine}"
+puts "---> PROXY COMMAND:\n" + cmd
+pid = spawn(cmd)
+Process.detach(pid)
