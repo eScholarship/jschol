58fd62bba65f20be216a9892172852e2d8c85563
EC2 Default User <ec2-user@ip-172-31-37-191.us-west-2.compute.internal>
2024-10-30 20:03:17 +0000

    Bumped node version in promoteVersion.sh

--- promoteVersion.sh
+++ promoteVersion.sh
@@ -20,12 +20,12 @@ if ! aws sts get-caller-identity > /dev/null 2>&1; then
 	exit 1 
 fi
 
-# Validate Node version is 18
-NODE_VERSION=$(node -v | awk -F'v' '{print $2}' | awk -F'.' '{print $1}')
+# Validate Node version is 20
+NODE_VERSION=$(node-20 -v | awk -F'v' '{print $2}' | awk -F'.' '{print $1}')
 
-# if NODE_VERSION is not equal to 18, exit
-if [[ $NODE_VERSION != 18 ]]; then
-  echo "Node version 18 is required."
+# if NODE_VERSION is not equal to 20, exit
+if [[ $NODE_VERSION != 20 ]]; then
+  echo "Node version 20 is required."
   exit 1
 fi
 
