821b74cd6b1e833690e2a4e0a60174e1245c2436
nicktahani <ns.tahani@gmail.com>
2025-03-13 10:35:34 -0700

    initial vite setup

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
+  resolve: {
+    alias: {
+      'pdfjs-lib': path.resolve(__dirname, 'node_modules/pdfjs-embed2/src/pdf.js'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    manifest: true,
+    minify: 'esbuild',
+  },
+  server: {
+    port: 18880,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:18880', 
+        changeOrigin: true,
+      }
+    },
+  },
+})
