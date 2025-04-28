a7b775c983574efb699d56425723c967bdae186f
nicktahani <ns.tahani@gmail.com>
2025-04-28 10:18:32 -0700

    debounce window resize

--- app/jsx/components/PdfViewWrapper.jsx
+++ app/jsx/components/PdfViewWrapper.jsx
@@ -1,15 +1,33 @@
 // this component exists as a functional wrapper for PdfViewComp,
 // since we want to use the resize observer hook
 
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import useResizeObserver from 'use-resize-observer'
 import PdfViewComp from '../components/PdfViewComp.jsx'
+import { debounce } from 'lodash'
 
-const PdfViewWrapper = (props) => {
+const PdfViewWrapper = props => {
   const { ref, width = 1 } = useResizeObserver()
-  console.log('resize width', width)
+  const [debouncedWidth, setDebouncedWidth] = useState(width)
 
-  return <PdfViewComp {...props} containerWidth={width} viewerRef={ref} />
+  // debounce and memoize the resize updates
+  const debouncedSetWidth = useCallback(
+    debounce(newWidth => {
+      setDebouncedWidth(newWidth)
+    }, 200), // every 200ms, dont need to update on every pixel change 
+    []
+  )
+  
+  // this effect is triggered when the width changes
+  useEffect(() => {
+    debouncedSetWidth(width)
+
+    return () => {
+      debouncedSetWidth.cancel()
+    }
+  }, [width, debouncedSetWidth])
+
+  return <PdfViewComp {...props} containerWidth={debouncedWidth} viewerRef={ref} />
 }
 
 export default PdfViewWrapper
