5059da19cd31ed6a5f9d596be541ff762e0cfcf3
nicktahani <ns.tahani@gmail.com>
2025-06-04 16:32:11 -0700

    WIP: require syntax conversion (CMS deps)

--- app/jsx/objects/TrumbowygObj.jsx
+++ app/jsx/objects/TrumbowygObj.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import trumbowyg from 'trumbowyg'
+import 'trumbowyg'
 const trumbowygIconsId = 'trumbowyg-icons'
 
 // Load some standard plugins
