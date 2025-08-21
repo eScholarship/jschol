086e0525971eac552dceb134fcc5a47647a6810b
nicktahani <ns.tahani@gmail.com>
2025-08-21 12:54:04 -0700

    expose jquery globally; fixes "not defined" error in trumbowyg

--- app/entry-client.jsx
+++ app/entry-client.jsx
@@ -4,6 +4,9 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './jsx/App.jsx'
+import jQuery from 'jquery'
+window.$ = jQuery
+window.jQuery = jQuery
 
 ReactDOM.hydrate(
   <StrictMode>
