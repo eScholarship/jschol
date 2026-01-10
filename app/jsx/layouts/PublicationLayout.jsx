b9a9b3ba70d3f769cfa024c6caeabc99f08d723c
nicktahani <ns.tahani@gmail.com>
2026-01-09 17:39:01 -0800

    cleanup: remove more warnings

--- app/jsx/layouts/PublicationLayout.jsx
+++ app/jsx/layouts/PublicationLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, browserHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import MarqueeComp from '../components/MarqueeComp.jsx'
 import JournalInfoComp from '../components/JournalInfoComp.jsx'
