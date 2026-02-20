e69e1d7e3dc5e0d75c5592ebf55f669a7d8dc1f7
nicktahani <ns.tahani@gmail.com>
2026-02-20 11:47:50 -0800

    convert LazyImageComp to a functional component; improve image load handling

--- app/jsx/components/LazyImageComp.jsx
+++ app/jsx/components/LazyImageComp.jsx
@@ -2,37 +2,40 @@
 
 // NOTE:
 // chrome is very efficient for a small quantity of images, so loading='lazy' doesn't always work
-// however, doing it this way eliminates the need for external dependencies 
+// however, using the native attribute eliminates the need for external dependencies 
 
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
-class LazyImageComp extends React.Component {
-  handleImageLoad = e => {
-    const image = e.target
-    image.setAttribute('data-loaded', 'true')
-  }
+function LazyImageComp({ src, alt, clickable }) {
+  const imgRef = useRef(null)
 
-  render() {
-    const { src, alt, clickable } = this.props
+  useEffect(() => {
+    const img = imgRef.current
+    // if the image was already cached, the onLoad event fires before react
+    // attaches its handler and is silently lost. check here as a fallback
+    if (img?.naturalWidth > 0) {
+      img.setAttribute('data-loaded', 'true')
+    }
+  }, [])
 
-    const imgElement = (
-      <img
-        className='c-lazyimage'
-        loading='lazy'
-        src={src}
-        alt={alt}
-        onLoad={this.handleImageLoad}
-      />
-    )
+  const imgElement = (
+    <img
+      ref={imgRef}
+      className='c-lazyimage'
+      loading='lazy'
+      src={src}
+      alt={alt}
+      onLoad={e => e.target.setAttribute('data-loaded', 'true')}
+    />
+  )
 
-    return clickable ? (
-      <a href={src} target='_blank'>
-        {imgElement}
-      </a>
-    ) : (
-      imgElement
-    )
-  }
+  return clickable ? (
+    <a href={src} target='_blank'>
+      {imgElement}
+    </a>
+  ) : (
+    imgElement
+  )
 }
 
 export default LazyImageComp;
