24c32f286225c3677c3b0196416aeedb033eb348
nicktahani <ns.tahani@gmail.com>
2026-03-24 13:40:49 -0700

    refactor JournalGridComp and add featured journals as a constant

---
+++ app/consts/featuredJournals.js
@@ -0,0 +1,7 @@
+export const featuredJournals = [
+  { path: '/uc/anrcs_californiaagriculture', image: 'homecover_CalAg.webp', alt: 'California Agriculture' },
+  { path: '/uc/psf', image: 'homecover_psf.webp', alt: 'Parks Stewardship Forum' },
+  { path: '/uc/jmie_sfews', image: 'homecover_sfews.webp', alt: 'San Francisco Estuary and Watershed Science' },
+  { path: '/uc/jrws', image: 'homecover_jrws.webp', alt: 'Journal of Right-Wing Studies' },
+  { path: '/uc/aicrj', image: 'homecover_aicrj.webp', alt: 'American Indian Culture and Research Journal' },
+]
--- app/jsx/components/JournalGridComp.jsx
+++ app/jsx/components/JournalGridComp.jsx
@@ -4,22 +4,21 @@ import React from 'react'
 import LazyImageComp from '../components/LazyImageComp.jsx'
 import { Link } from 'react-router-dom'
 import MEDIA_PATH from '../../js/MediaPath.js'
+import { featuredJournals } from '../../consts/featuredJournals.js'
 
-class JournalGridComp extends React.Component {
-  render() {
-    return (
-      <div className="c-journalgrid">
-        <Link to="/uc/anrcs_californiaagriculture"><LazyImageComp src={MEDIA_PATH + 'homecover_CalAg.webp'} alt="California Agriculture" /></Link>
-        <Link to="/uc/psf"><LazyImageComp src={MEDIA_PATH + 'homecover_psf.webp'} alt="Parks Stewardship Forum"/></Link>
-        <Link to="/uc/jmie_sfews"><LazyImageComp src={MEDIA_PATH + 'homecover_sfews.webp'} alt="San Francisco Estuary and Watershed Science"/></Link>
-        <Link to="/uc/jrws"><LazyImageComp src={MEDIA_PATH + 'homecover_jrws.webp'} alt="Journal of Right-Wing Studies"/></Link>
-        <Link to="/uc/aicrj"><LazyImageComp src={MEDIA_PATH + 'homecover_aicrj.webp'} alt="American Indian Culture and Research Journal"/></Link>
-        <div className="c-journalgrid__all">
-          <Link to="/journals">View all <strong>{this.props.count_journals}</strong> current journals</Link>
-        </div>
+function JournalGridComp({ count_journals }) {
+  return (
+    <div className="c-journalgrid">
+      {featuredJournals.map(({ path, image, alt }) => (
+        <Link key={path} to={path}>
+          <LazyImageComp src={MEDIA_PATH + image} alt={alt} />
+        </Link>
+      ))}
+      <div className="c-journalgrid__all">
+        <Link to="/journals">View all <strong>{count_journals}</strong> current journals</Link>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default JournalGridComp;
