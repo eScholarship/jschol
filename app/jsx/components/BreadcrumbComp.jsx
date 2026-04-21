4064f0d9036a6b36130ed353ad0c5547e8d1d638
nicktahani <ns.tahani@gmail.com>
2026-04-21 15:03:00 -0700

    convert BreadcrumbComp to functional component

--- app/jsx/components/BreadcrumbComp.jsx
+++ app/jsx/components/BreadcrumbComp.jsx
@@ -3,30 +3,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-class BreadcrumbComp extends React.Component {
-  render() {
-    const { array } = this.props
-    if (!array || array.length === 0) return null
+const BreadcrumbComp = ({ array }) => {
+  if (!array?.length) return null
 
-    const lastN = array.length - 1
-    const nodes = array.map((node, i) => (
-      <li key={i}>
-        <Link 
-          className={i === lastN ? "c-breadcrumb-link--active" : null} 
-          to={node.url}
-          aria-current={i === lastN ? "page" : undefined}
-        >
-          {node.name}
-        </Link>
-      </li>
-    ))
+  const lastN = array.length - 1
 
-    return (
-      <nav className="c-breadcrumb" aria-label="breadcrumb">
-        <ul>{nodes}</ul>
-      </nav>
-    )
-  }
+  return (
+    <nav className="c-breadcrumb" aria-label="breadcrumb">
+      <ul>
+        {array.map((node, i) => (
+          <li key={i}>
+            <Link
+              className={i === lastN ? "c-breadcrumb-link--active" : null}
+              to={node.url}
+              aria-current={i === lastN ? "page" : undefined}
+            >
+              {node.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
 }
 
-export default BreadcrumbComp;
+export default BreadcrumbComp
