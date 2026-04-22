ff86e371e65c6b1ae9d27785fe90adb2d5713542
nicktahani <ns.tahani@gmail.com>
2026-04-22 13:58:38 -0700

    update "Main Content" tab focus management (22)

--- app/jsx/components/TabsComp.jsx
+++ app/jsx/components/TabsComp.jsx
@@ -1,7 +1,6 @@
 // ##### Tabs Component ##### //
 
 import React from 'react'
-import $ from 'jquery'
 import Contexts from '../contexts.jsx'
 import TabMainComp from '../components/TabMainComp.jsx'
 import TabSupplementalComp from '../components/TabSupplementalComp.jsx'
@@ -11,15 +10,24 @@ import TabMetaComp from '../components/TabMetaComp.jsx'
 
 class TabsComp extends React.Component {
   state = {moreTabs: false}
+  tabContentRef = React.createRef()
 
   tabFocus(tabName) {
     this.props.changeTab(tabName)
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (this.props.currentTab != nextProps.currentTab) { 
-      // For keyboard users, jump to heading within the tab.
-      setTimeout(() =>$(".c-tabcontent__main-heading").focus(), 0)
+  componentDidUpdate(prevProps) {
+    if (prevProps.currentTab != this.props.currentTab) {
+      // For keyboard users, focus the heading at the start of the newly active tab panel
+      const container = this.tabContentRef.current
+      if (container) {
+        const heading = container.querySelector(".c-tabcontent__main-heading")
+        if (heading) {
+          heading.focus()
+        } else {
+          container.focus()
+        }
+      }
     }
   }
 
@@ -69,7 +77,7 @@ class TabsComp extends React.Component {
         }
       </Contexts.CMS.Consumer>
         </div>
-        <div className="c-tabs__content">
+        <div className="c-tabs__content" ref={this.tabContentRef} tabIndex="-1">
           {/* 'tab_anchors' are defined in ItemPage component */}
           {p.currentTab == "main"         && <TabMainComp {...p} />}
           {p.currentTab == "supplemental" && <TabSupplementalComp {...p} />}
