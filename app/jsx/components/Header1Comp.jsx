c53815b586870d6dada341771f5fc33bb7b1180a
nicktahani <ns.tahani@gmail.com>
2026-07-10 11:41:01 -0700

    refactor notification banner and render

--- app/jsx/components/Header1Comp.jsx
+++ app/jsx/components/Header1Comp.jsx
@@ -6,6 +6,7 @@ import Breakpoints from '../../js/breakpoints.json'
 import Search1Comp from '../components/Search1Comp.jsx'
 import AdminBarComp from '../components/AdminBarComp.jsx'
 import MEDIA_PATH from '../../js/MediaPath.js'
+import NotificationComp from '../components/NotificationComp.jsx'
 
 class HeaderComp1 extends React.Component {
   state = {searchActive: false}
@@ -25,6 +26,18 @@ class HeaderComp1 extends React.Component {
         </div>
         <button className="c-header__search-open-button" aria-label="open search field" onClick = {()=> this.setState({searchActive: true})}></button>
       </header>
+      <NotificationComp>
+        <span>
+          Note: the eScholarship site's performance may be inconsistent July 13–24. {" "}
+          <a href="https://help.escholarship.org/support/discussions/topics/9000064214">
+            (Learn more).
+          </a>
+        </span>
+        <br />
+        <span>
+          Please <a href="https://help.escholarship.org/support/discussions/topics/9000064214">provide feedback</a> if you encounter any issues.  
+        </span>
+      </NotificationComp>
     </div>
     )
   }
--- app/jsx/components/NotificationComp.jsx
+++ app/jsx/components/NotificationComp.jsx
@@ -1,16 +1,16 @@
 // ##### Notification Component ##### //
-import React from 'react'
-class NotificationComp extends React.Component {
-  constructor(){
-    super()
-    this.state = {messageClose: false}
-  }
-  render() {
-    return (
-      <div className={this.state.messageClose ? "c-notification--close" : "c-notification"} role="alert" hidden={this.state.messageClose ? true : false}>
-        <strong className="c-notification__main-text"></strong>
-      </div>
-    )
-  }
+import React, { useState } from 'react'
+
+function NotificationComp({ children }) {
+  const [messageClose, setMessageClose] = useState(false)
+  return (
+    <div className={messageClose ? "c-notification--close" : "c-notification"} role="alert" hidden={messageClose}>
+      <p>
+        <strong className="c-notification__main-text">{children}</strong>
+      </p>
+      <a className="c-notification__close" role="button" aria-label="Close notification" onClick={() => setMessageClose(true)}></a>
+    </div>
+  )
 }
+
 export default NotificationComp;
