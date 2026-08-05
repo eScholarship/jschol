0985879d83067aae6ae01ba68f084fc8645a9dfe
nicktahani <ns.tahani@gmail.com>
2026-08-05 11:29:26 -0700

    remove notification banner

--- app/jsx/components/Header1Comp.jsx
+++ app/jsx/components/Header1Comp.jsx
@@ -6,7 +6,7 @@ import Breakpoints from '../../js/breakpoints.json'
 import Search1Comp from '../components/Search1Comp.jsx'
 import AdminBarComp from '../components/AdminBarComp.jsx'
 import MEDIA_PATH from '../../js/MediaPath.js'
-import NotificationComp from '../components/NotificationComp.jsx'
+// import NotificationComp from '../components/NotificationComp.jsx'
 
 class HeaderComp1 extends React.Component {
   state = {searchActive: false}
@@ -26,7 +26,7 @@ class HeaderComp1 extends React.Component {
         </div>
         <button className="c-header__search-open-button" aria-label="open search field" onClick = {()=> this.setState({searchActive: true})}></button>
       </header>
-      <NotificationComp>
+      {/* <NotificationComp>
         <span>
           Note: the eScholarship site's performance may be inconsistent July 13–30 {" "}
           <a href="https://help.escholarship.org/support/discussions/topics/9000064214">
@@ -37,7 +37,7 @@ class HeaderComp1 extends React.Component {
         <span>
           {" "} Please <a href="https://help.escholarship.org/support/discussions/topics/9000064214">provide feedback</a> if you encounter any issues.  
         </span>
-      </NotificationComp>
+      </NotificationComp> */}
     </div>
     )
   }
